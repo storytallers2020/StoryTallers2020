5637718e9f4f881c80985b66a1c91b914d985f0c
Vitas1968 <dor1968@mail.ru>
2020-06-16 19:59:31 +0700

    добавлены базовые свойства класса Story

--- model/src/main/java/com/google/vitaly/model/userdata/Story.kt
+++ model/src/main/java/com/google/vitaly/model/userdata/Story.kt
@@ -1,4 +1,8 @@
 package com.google.vitaly.model.userdata
 
-class Story {
-}
+class Story(
+    val storyId: Int?,
+    val storyName: String?,
+    val storyLocation: Location?,
+    val storyPersonage: List<Personage> = listOf()
+)
