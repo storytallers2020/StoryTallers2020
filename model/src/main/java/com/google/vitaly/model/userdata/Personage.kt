9b4202179e2bfae8fea942290ea69a0905f2e149
Vitas1968 <dor1968@mail.ru>
2020-06-23 17:54:22 +0700

    переименовал переменные

--- model/src/main/java/com/google/vitaly/model/userdata/Personage.kt
+++ model/src/main/java/com/google/vitaly/model/userdata/Personage.kt
@@ -1,7 +1,7 @@
 package com.google.vitaly.model.userdata
 
 data class Personage(
-    val persId: Int?,
-    val persName: String?,
-    val persAvatarUrl: String?
+    val id: Int?,
+    val name: String?,
+    val avatarUrl: String?
 )
