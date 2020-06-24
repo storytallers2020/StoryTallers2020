b707afa19be195bb471c7d9cee1ae4b31ee74a62
Vitas1968 <dor1968@mail.ru>
2020-06-24 22:06:13 +0700

    добавил экран CreateCharacterScreen

--- app/src/main/java/ru/storytallers/navigation/Screens.kt
+++ app/src/main/java/ru/storytallers/navigation/Screens.kt
@@ -1,12 +1,15 @@
 package ru.storytallers.navigation
 
 import androidx.fragment.app.Fragment
+import ru.storytallers.ui.fragments.AuthFragment
+import ru.storytallers.ui.fragments.CreateCharacterFragment
 import ru.terrakok.cicerone.android.support.SupportAppScreen
 
 sealed class Screens {
     class AuthScreen : SupportAppScreen(){
-        override fun getFragment(): Fragment {
-            return super.getFragment()
-        }
+        override fun getFragment()= AuthFragment.newInstance()
+    }
+    class CreateCharacterScreen : SupportAppScreen(){
+        override fun getFragment()= CreateCharacterFragment.newInstance()
     }
 }
