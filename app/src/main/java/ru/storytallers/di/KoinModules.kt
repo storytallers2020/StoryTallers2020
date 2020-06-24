7f325df27551ca6d4c871c117224f74f3fc7e14c
Vitas1968 <dor1968@mail.ru>
2020-06-24 22:04:28 +0700

    внедрение Cicerone, роутера и холдера для навигации

--- app/src/main/java/ru/storytallers/di/KoinModules.kt
+++ app/src/main/java/ru/storytallers/di/KoinModules.kt
@@ -1,2 +1,17 @@
 package ru.storytallers.di
 
+import org.koin.core.context.loadKoinModules
+import org.koin.dsl.module
+import ru.terrakok.cicerone.Cicerone
+import ru.terrakok.cicerone.Router
+
+fun injectDependencies() = loadModules
+private val loadModules by lazy {
+    loadKoinModules(listOf(application)) //, mainScreen
+}
+
+val application = module {
+    single { Cicerone.create() }
+    single { get<Cicerone<Router>>().router}
+    single { get<Cicerone<Router>>().navigatorHolder}
+}
