312758f959e7ae5b4313298a3f1f43a0c3737fe9
Vitas1968 <dor1968@mail.ru>
2020-06-24 19:32:20 +0700

    переопределил переменные из базового класса

--- app/src/main/java/ru/storytallers/ui/fragments/AuthFragment.kt
+++ app/src/main/java/ru/storytallers/ui/fragments/AuthFragment.kt
@@ -1,6 +1,12 @@
 package ru.storytallers.ui.fragments
 
+import ru.storytallers.R
 import ru.storytallers.ui.fragments.basefragment.BaseFragment
+import ru.storytallers.viewmodels.AuthViewModel
+import ru.storytallers.viewmodels.baseviewmodel.BaseViewModel
+import ru.storytellers.model.DataModel
 
-class AuthFragment: BaseFragment() {
+class AuthFragment: BaseFragment<DataModel>() {
+    override val layoutRes = R.layout.fragment_auth
+    override lateinit var model: AuthViewModel
 }
