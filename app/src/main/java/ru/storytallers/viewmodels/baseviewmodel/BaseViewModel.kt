e6e2b6b24bcfe792c7292ca4826c464298d10db4
Vitas1968 <dor1968@mail.ru>
2020-06-24 17:05:37 +0700

    создание класса

--- app/src/main/java/ru/storytallers/viewmodels/baseviewmodel/BaseViewModel.kt
+++ app/src/main/java/ru/storytallers/viewmodels/baseviewmodel/BaseViewModel.kt
@@ -1,4 +1,10 @@
 package ru.storytallers.viewmodels.baseviewmodel
 
-class BaseViewModel {
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import ru.storytellers.model.DataModel
+
+abstract class BaseViewModel<T : DataModel>(
+    protected open val _mutableLiveData: MutableLiveData<T> = MutableLiveData()
+): ViewModel() {
 }
