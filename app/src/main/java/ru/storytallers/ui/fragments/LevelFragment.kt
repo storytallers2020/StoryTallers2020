738d916e90e83df4e0c4f677be38fd84e2308c7f
Vitas1968 <dor1968@mail.ru>
2020-06-24 22:41:01 +0700

    базовая реализация LevelFragment

--- app/src/main/java/ru/storytallers/ui/fragments/LevelFragment.kt
+++ app/src/main/java/ru/storytallers/ui/fragments/LevelFragment.kt
@@ -3,12 +3,13 @@ package ru.storytallers.ui.fragments
 import ru.storytallers.R
 import ru.storytallers.ui.fragments.basefragment.BaseFragment
 import ru.storytallers.viewmodels.CreateCharacterViewModel
+import ru.storytallers.viewmodels.LevelViewModel
 import ru.storytellers.model.DataModel
 
-class CreateCharacterFragment: BaseFragment<DataModel>() {
-    override lateinit var model: CreateCharacterViewModel
-    override val layoutRes= R.layout.fragment_character_create
+class LevelFragment: BaseFragment<DataModel>() {
+    override lateinit var model: LevelViewModel
+    override val layoutRes= R.layout.fragment_level
     companion object {
-        fun newInstance() = CreateCharacterFragment()
+        fun newInstance() = LevelFragment()
     }
 }
