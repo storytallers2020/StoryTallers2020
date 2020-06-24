378b4ea4307c9ef16bebb612aa9f17d8768b1bb2
Vitas1968 <dor1968@mail.ru>
2020-06-24 22:43:05 +0700

    добавил экран LevelScreen

--- app/src/main/java/ru/storytallers/navigation/Screens.kt
+++ app/src/main/java/ru/storytallers/navigation/Screens.kt
@@ -1,8 +1,8 @@
 package ru.storytallers.navigation
 
-import androidx.fragment.app.Fragment
 import ru.storytallers.ui.fragments.AuthFragment
 import ru.storytallers.ui.fragments.CreateCharacterFragment
+import ru.storytallers.ui.fragments.LevelFragment
 import ru.terrakok.cicerone.android.support.SupportAppScreen
 
 sealed class Screens {
@@ -11,5 +11,9 @@ sealed class Screens {
     }
     class CreateCharacterScreen : SupportAppScreen(){
         override fun getFragment()= CreateCharacterFragment.newInstance()
+    }
+
+    class LevelScreen : SupportAppScreen(){
+        override fun getFragment()= LevelFragment.newInstance()
     }
 }
