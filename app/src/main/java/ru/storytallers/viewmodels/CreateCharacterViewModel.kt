eb7438421df743053e760ad1d42ae56e07a63609
Vitas1968 <dor1968@mail.ru>
2020-06-24 22:00:32 +0700

    создал фрагмет CreateCharacterFragment с моделью

---
+++ app/src/main/java/ru/storytallers/viewmodels/CreateCharacterViewModel.kt
@@ -0,0 +1,12 @@
+package ru.storytallers.viewmodels
+
+
+import androidx.lifecycle.LiveData
+import ru.storytallers.viewmodels.baseviewmodel.BaseViewModel
+import ru.storytellers.model.DataModel
+
+class AuthViewModel : BaseViewModel<DataModel>() {
+    private val liveDataForViewToObserve: LiveData<DataModel> = _mutableLiveData
+    }
+
+
