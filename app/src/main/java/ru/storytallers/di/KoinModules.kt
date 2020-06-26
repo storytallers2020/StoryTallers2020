532cde4e5b03b2138a5c705921183c1c5d5c4fe7
LeonovEgor <leonov.egor@gmail.com>
2020-06-26 11:49:15 +0300

    Добавил подключение БД в Koin

--- app/src/main/java/ru/storytallers/di/KoinModules.kt
+++ app/src/main/java/ru/storytallers/di/KoinModules.kt
@@ -1,17 +1,31 @@
 package ru.storytallers.di
 
+import androidx.room.Room
 import org.koin.core.context.loadKoinModules
 import org.koin.dsl.module
+import ru.storytellers.model.entity.room.db.AppDatabase
 import ru.terrakok.cicerone.Cicerone
 import ru.terrakok.cicerone.Router
 
 fun injectDependencies() = loadModules
 private val loadModules by lazy {
-    loadKoinModules(listOf(application)) //, mainScreen
+    loadKoinModules(listOf(application, database)) //, mainScreen
 }
 
 val application = module {
     single { Cicerone.create() }
     single { get<Cicerone<Router>>().router}
     single { get<Cicerone<Router>>().navigatorHolder}
+}
+
+val database = module {
+    single {
+        Room.databaseBuilder(get(), AppDatabase::class.java, "StoryTallersDB")
+            .build()
+    }
+    single { get<AppDatabase>().characterDao }
+    single { get<AppDatabase>().sentenceOfTaleDao }
+    single { get<AppDatabase>().locationDao }
+    single { get<AppDatabase>().storyDao }
+    single { get<AppDatabase>().userDao }
 }
