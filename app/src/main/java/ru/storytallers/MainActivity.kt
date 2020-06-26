73f6656e93cf305aca010b5d607fc6d2313c20bb
Storytallers2020 <storytallers2020@mail.ru>
2020-06-26 14:06:02 +0300

    Был пулл без коммита, исправил

--- app/src/main/java/ru/storytallers/MainActivity.kt
+++ app/src/main/java/ru/storytallers/MainActivity.kt
@@ -8,13 +8,5 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.main_activity)
-        /*
-        if (savedInstanceState == null) {
-            supportFragmentManager.beginTransaction()
-                    .replace(R.id.container, MainFragment.newInstance())
-                    .commitNow()
-        }
-        
-         */
     }
 }
