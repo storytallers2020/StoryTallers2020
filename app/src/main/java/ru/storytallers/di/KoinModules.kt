121a40e487f5674406238a43af4b93749c62d0d6
Vitas1968 <dor1968@mail.ru>
2020-06-26 16:49:53 +0700

    подчистил

--- app/src/main/java/ru/storytallers/di/KoinModules.kt
+++ app/src/main/java/ru/storytallers/di/KoinModules.kt
@@ -6,12 +6,13 @@ import ru.terrakok.cicerone.Cicerone
 import ru.terrakok.cicerone.Router
 
 fun injectDependencies() = loadModules
+
 private val loadModules by lazy {
-    loadKoinModules(listOf(application)) //, mainScreen
+    loadKoinModules(listOf(application))
 }
 
 val application = module {
     single { Cicerone.create() }
-    single { get<Cicerone<Router>>().router}
-    single { get<Cicerone<Router>>().navigatorHolder}
+    single { get<Cicerone<Router>>().router }
+    single { get<Cicerone<Router>>().navigatorHolder }
 }
