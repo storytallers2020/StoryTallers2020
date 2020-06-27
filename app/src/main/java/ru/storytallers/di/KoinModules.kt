301923e487ebada95b501d3718da33eb3e0f3117
Vitas1968 <dor1968@mail.ru>
2020-06-27 16:18:32 +0700

    внедрение для viewmodel StartFragment

--- app/src/main/java/ru/storytallers/di/KoinModules.kt
+++ app/src/main/java/ru/storytallers/di/KoinModules.kt
@@ -1,21 +1,31 @@
 package ru.storytallers.di
 
 import androidx.room.Room
+import org.koin.android.viewmodel.dsl.viewModel
 import org.koin.core.context.loadKoinModules
+import org.koin.core.qualifier.named
 import org.koin.dsl.module
+import ru.storytallers.ui.fragments.StartFragment
+import ru.storytallers.viewmodels.StartViewModel
 import ru.storytellers.model.entity.room.db.AppDatabase
 import ru.terrakok.cicerone.Cicerone
 import ru.terrakok.cicerone.Router
 
 fun injectDependencies() = loadModules
 private val loadModules by lazy {
-    loadKoinModules(listOf(application, database)) //, mainScreen
+    loadKoinModules(listOf(application, database,startmodel))
 }
 
 val application = module {
     single { Cicerone.create() }
     single { get<Cicerone<Router>>().router}
     single { get<Cicerone<Router>>().navigatorHolder}
+}
+
+val startmodel =  module {
+    scope(named<StartFragment>()) {
+        viewModel { StartViewModel() }
+    }
 }
 
 val database = module {
