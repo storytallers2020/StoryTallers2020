738ece8d0f6b87c7a87de7c55307c21185c3a57a
Vitas1968 <dor1968@mail.ru>
2020-06-27 16:16:25 +0700

    вынес в базовый класс фрагментов компоненты навигации

--- app/src/main/java/ru/storytallers/ui/fragments/basefragment/BaseFragment.kt
+++ app/src/main/java/ru/storytallers/ui/fragments/basefragment/BaseFragment.kt
@@ -5,19 +5,32 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import ru.storytallers.R
+import org.koin.android.ext.android.inject
+import ru.storytallers.ui.BackButtonListener
 import ru.storytallers.viewmodels.baseviewmodel.BaseViewModel
 import ru.storytellers.model.DataModel
+import ru.terrakok.cicerone.NavigatorHolder
+import ru.terrakok.cicerone.Router
 
 
-abstract class BaseFragment<T:DataModel>: Fragment() {
+abstract class BaseFragment<T:DataModel>: Fragment(), BackButtonListener {
  abstract val model: BaseViewModel<T>
   abstract val layoutRes: Int
+ lateinit var navigatorHolder: NavigatorHolder
+  lateinit var router: Router
  override fun onCreateView(
   inflater: LayoutInflater,
   container: ViewGroup?,
   savedInstanceState: Bundle?
  ): View? {
+  injectRouter()
   return inflater.inflate(layoutRes, container, false)
+ }
+
+ private fun injectRouter(){
+  val navigHold: NavigatorHolder by inject()
+  navigatorHolder=navigHold
+  val rout: Router by inject()
+  router=rout
  }
  }
