95fb3e5a5a745c96d7560ce56744ced62bf19b41
Vitas1968 <dor1968@mail.ru>
2020-06-27 16:25:57 +0700

    методы прямой и обратной навигации

--- app/src/main/java/ru/storytallers/ui/fragments/StartFragment.kt
+++ app/src/main/java/ru/storytallers/ui/fragments/StartFragment.kt
@@ -1,14 +1,33 @@
 package ru.storytallers.ui.fragments
 
+import android.os.Bundle
+import android.view.View
+import com.google.android.material.button.MaterialButton
 import ru.storytallers.R
 import ru.storytallers.ui.fragments.basefragment.BaseFragment
-import ru.storytallers.viewmodels.AuthViewModel
+import ru.storytallers.viewmodels.StartViewModel
 import ru.storytellers.model.DataModel
+import kotlinx.android.synthetic.main.fragment_start.start_button
+import ru.storytallers.navigation.Screens
 
 class StartFragment: BaseFragment<DataModel>() {
     override val layoutRes = R.layout.fragment_start
-    override lateinit var model: AuthViewModel
+    override lateinit var model: StartViewModel
+    private lateinit var startButton: MaterialButton
+
     companion object {
         fun newInstance() = StartFragment()
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        startButton=start_button
+        startButton.setOnClickListener{
+            router.replaceScreen(Screens.LevelScreen())
+        }
+    }
+
+    override fun backClicked(): Boolean {
+        TODO("Not yet implemented")
     }
 }
