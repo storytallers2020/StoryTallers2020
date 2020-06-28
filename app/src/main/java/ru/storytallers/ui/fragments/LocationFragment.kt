0587f3978b321815c0ff1bad8f2b41f78b59179f
Vitas1968 <dor1968@mail.ru>
2020-06-28 17:45:29 +0700

    созданы фрагмент и модель для экрана Location

---
+++ app/src/main/java/ru/storytallers/ui/fragments/LocationFragment.kt
@@ -0,0 +1,44 @@
+package ru.storytallers.ui.fragments
+
+import android.os.Bundle
+import android.view.View
+import androidx.lifecycle.Observer
+import com.google.android.material.button.MaterialButton
+import ru.storytallers.R
+import ru.storytallers.ui.fragments.basefragment.BaseFragment
+import ru.storytallers.viewmodels.StartViewModel
+import ru.storytellers.model.DataModel
+import kotlinx.android.synthetic.main.fragment_start.start_button
+import org.koin.android.scope.currentScope
+import ru.storytallers.navigation.Screens
+
+class StartFragment: BaseFragment<DataModel>() {
+    override val layoutRes = R.layout.fragment_start
+    override lateinit var model: StartViewModel
+    private lateinit var startButton: MaterialButton
+
+    companion object {
+        fun newInstance() = StartFragment()
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        iniViewModel()
+        startButton=start_button
+        startButton.setOnClickListener{
+            router.replaceScreen(Screens.LevelScreen())
+        }
+    }
+
+    private fun iniViewModel() {
+        val viewModel: StartViewModel by currentScope.inject()
+        model = viewModel
+        model.subscribe().observe(viewLifecycleOwner, Observer<DataModel> {
+
+        } )
+    }
+
+    override fun backClicked(): Boolean {
+        TODO("Not yet implemented")
+    }
+}
