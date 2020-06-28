3176427c286546e2579bc70eb88556ea7d5fbd94
Vitas1968 <dor1968@mail.ru>
2020-06-28 17:54:59 +0700

    реализация фрагмента Location

--- app/src/main/java/ru/storytallers/ui/fragments/LocationFragment.kt
+++ app/src/main/java/ru/storytallers/ui/fragments/LocationFragment.kt
@@ -3,38 +3,30 @@ package ru.storytallers.ui.fragments
 import android.os.Bundle
 import android.view.View
 import androidx.lifecycle.Observer
-import com.google.android.material.button.MaterialButton
 import ru.storytallers.R
 import ru.storytallers.ui.fragments.basefragment.BaseFragment
-import ru.storytallers.viewmodels.StartViewModel
 import ru.storytellers.model.DataModel
-import kotlinx.android.synthetic.main.fragment_start.start_button
 import org.koin.android.scope.currentScope
-import ru.storytallers.navigation.Screens
+import ru.storytallers.viewmodels.LocationViewModel
 
-class StartFragment: BaseFragment<DataModel>() {
-    override val layoutRes = R.layout.fragment_start
-    override lateinit var model: StartViewModel
-    private lateinit var startButton: MaterialButton
+class LocationFragment: BaseFragment<DataModel>() {
+    override val layoutRes = R.layout.fragment_location
+    override lateinit var model: LocationViewModel
+
 
     companion object {
-        fun newInstance() = StartFragment()
+        fun newInstance() = LocationFragment()
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         iniViewModel()
-        startButton=start_button
-        startButton.setOnClickListener{
-            router.replaceScreen(Screens.LevelScreen())
-        }
     }
 
-    private fun iniViewModel() {
-        val viewModel: StartViewModel by currentScope.inject()
+    override fun iniViewModel() {
+        val viewModel: LocationViewModel by currentScope.inject()
         model = viewModel
         model.subscribe().observe(viewLifecycleOwner, Observer<DataModel> {
-
         } )
     }
 
