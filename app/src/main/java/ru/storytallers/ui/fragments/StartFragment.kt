aa49f24848d6f09c3ff252f957f27525b38ed76a
Vitas1968 <dor1968@mail.ru>
2020-06-28 17:56:56 +0700

    внедрение модели и подписка на нее а так же заменил способ навигации

--- app/src/main/java/ru/storytallers/ui/fragments/StartFragment.kt
+++ app/src/main/java/ru/storytallers/ui/fragments/StartFragment.kt
@@ -2,12 +2,14 @@ package ru.storytallers.ui.fragments
 
 import android.os.Bundle
 import android.view.View
+import androidx.lifecycle.Observer
 import com.google.android.material.button.MaterialButton
 import ru.storytallers.R
 import ru.storytallers.ui.fragments.basefragment.BaseFragment
 import ru.storytallers.viewmodels.StartViewModel
 import ru.storytellers.model.DataModel
 import kotlinx.android.synthetic.main.fragment_start.start_button
+import org.koin.android.scope.currentScope
 import ru.storytallers.navigation.Screens
 
 class StartFragment: BaseFragment<DataModel>() {
@@ -21,10 +23,19 @@ class StartFragment: BaseFragment<DataModel>() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        iniViewModel()
         startButton=start_button
         startButton.setOnClickListener{
-            router.replaceScreen(Screens.LevelScreen())
+            router.navigateTo(Screens.LevelScreen())
+
         }
+    }
+
+    override fun iniViewModel() {
+        val viewModel: StartViewModel by currentScope.inject()
+        model = viewModel
+        model.subscribe().observe(viewLifecycleOwner, Observer<DataModel> {
+        } )
     }
 
     override fun backClicked(): Boolean {
