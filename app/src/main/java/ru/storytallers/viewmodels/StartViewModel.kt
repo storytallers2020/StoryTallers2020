7b28206d3431ac505853825f0cc813586bf779ed
Vitas1968 <dor1968@mail.ru>
2020-06-28 17:57:24 +0700

    добавлен метод подписки

--- app/src/main/java/ru/storytallers/viewmodels/StartViewModel.kt
+++ app/src/main/java/ru/storytallers/viewmodels/StartViewModel.kt
@@ -7,6 +7,12 @@ import ru.storytellers.model.DataModel
 
 class StartViewModel : BaseViewModel<DataModel>() {
     private val liveDataForViewToObserve: LiveData<DataModel> = _mutableLiveData
+
+    override fun subscribe(): LiveData<DataModel> {
+        return liveDataForViewToObserve
     }
+
+
+}
 
 
