ece1ab560ed5f34fdb8b9730c80b7a7515a4f3ec
Vitas1968 <dor1968@mail.ru>
2020-06-28 17:54:05 +0700

    реализация метода подписки

--- app/src/main/java/ru/storytallers/viewmodels/LevelViewModel.kt
+++ app/src/main/java/ru/storytallers/viewmodels/LevelViewModel.kt
@@ -7,6 +7,10 @@ import ru.storytellers.model.DataModel
 
 class LevelViewModel : BaseViewModel<DataModel>() {
     private val liveDataForViewToObserve: LiveData<DataModel> = _mutableLiveData
+
+    override fun subscribe(): LiveData<DataModel> {
+        return liveDataForViewToObserve
     }
+}
 
 
