ffef9cf53e83b6e6a3909cb6c8075592db24e3ce
Vitas1968 <dor1968@mail.ru>
2020-06-28 17:58:02 +0700

    добавлен метод подписки

--- app/src/main/java/ru/storytallers/viewmodels/LocationViewModel.kt
+++ app/src/main/java/ru/storytallers/viewmodels/LocationViewModel.kt
@@ -5,8 +5,11 @@ import androidx.lifecycle.LiveData
 import ru.storytallers.viewmodels.baseviewmodel.BaseViewModel
 import ru.storytellers.model.DataModel
 
-class LevelViewModel : BaseViewModel<DataModel>() {
+class LocationViewModel : BaseViewModel<DataModel>() {
     private val liveDataForViewToObserve: LiveData<DataModel> = _mutableLiveData
+    public override fun subscribe(): LiveData<DataModel> {
+        return liveDataForViewToObserve
     }
+}
 
 
