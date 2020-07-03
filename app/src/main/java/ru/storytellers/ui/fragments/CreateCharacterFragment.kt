cea4aae7af6781c64fa6604918b7d0333a4ae9da
Vitas1968 <dor1968@mail.ru>
2020-07-03 19:01:46 +0700

    инициализация рециклера и адаптера

--- app/src/main/java/ru/storytellers/ui/fragments/CreateCharacterFragment.kt
+++ app/src/main/java/ru/storytellers/ui/fragments/CreateCharacterFragment.kt
@@ -1,16 +1,23 @@
 package ru.storytellers.ui.fragments
 
 import androidx.lifecycle.Observer
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import androidx.recyclerview.widget.StaggeredGridLayoutManager
 import kotlinx.android.synthetic.main.fragment_character_create.*
 import org.koin.android.scope.currentScope
 import ru.storytellers.R
 import ru.storytellers.model.DataModel
 import ru.storytellers.navigation.Screens
+import ru.storytellers.ui.adapters.ChooseCharacterAdapter
 import ru.storytellers.ui.fragments.basefragment.BaseFragment
+import ru.storytellers.utils.viewById
 import ru.storytellers.viewmodels.CreateCharacterViewModel
 
 class CreateCharacterFragment: BaseFragment<DataModel>() {
     override lateinit var model: CreateCharacterViewModel
+    private val characterRecyclerView by viewById<RecyclerView>(R.id.rv_characters)
+    private val characterAdapter: ChooseCharacterAdapter by lazy { ChooseCharacterAdapter() }
     override val layoutRes= R.layout.fragment_character_create
 
     companion object {
@@ -30,9 +37,21 @@ class CreateCharacterFragment: BaseFragment<DataModel>() {
     }
 
     override fun init() {
-        btn_next.setOnClickListener {
-            router.navigateTo(Screens.LocationlScreen())
-        }
+        btn_next.setOnClickListener { navigateToLocationScreen() }
         back_button_character.setOnClickListener {backClicked()}
+        initRecycler()
+    }
+
+    private fun navigateToLocationScreen() {
+        router.navigateTo(Screens.LocationlScreen())
+    }
+
+    private fun initRecycler(){
+       val staggeredGridLayoutManager = StaggeredGridLayoutManager(
+           2,
+           LinearLayoutManager.HORIZONTAL)
+           .apply {reverseLayout=true}
+        rv_characters.layoutManager=staggeredGridLayoutManager
+        rv_characters.adapter=characterAdapter
     }
 }
