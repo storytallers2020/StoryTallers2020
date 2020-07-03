7deb36273e395f8add808e44041487d3b253f7c1
LeonovEgor <leonov.egor@gmail.com>
2020-07-03 09:21:21 +0300

    Добавил game в DI и App

--- app/src/main/java/ru/storytellers/application/StoryTallerApp.kt
+++ app/src/main/java/ru/storytellers/application/StoryTallerApp.kt
@@ -1,13 +1,28 @@
 package ru.storytellers.application
 
 import android.app.Application
+import org.koin.android.ext.android.inject
 import org.koin.android.ext.koin.androidContext
 import org.koin.core.context.startKoin
+import ru.storytellers.engine.Game
+import ru.storytellers.engine.level.Levels
 import timber.log.Timber
 
 class StoryTallerApp: Application() {
+
+    companion object {
+        lateinit var instance: StoryTallerApp
+            private set
+    }
+
+    val levels: Levels by inject()
+    val game: Game by inject()
+
     override fun onCreate() {
         super.onCreate()
+
+        instance = this
+
         startKoin { androidContext(this@StoryTallerApp) }
         Timber.plant(Timber.DebugTree())
     }
