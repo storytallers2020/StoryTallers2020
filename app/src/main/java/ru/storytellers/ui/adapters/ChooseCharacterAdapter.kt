867f30cf44f7461e19efe8b7ab7d22894b7b9329
Vitas1968 <dor1968@mail.ru>
2020-07-03 19:02:10 +0700

    шаблон адаптера

--- app/src/main/java/ru/storytellers/ui/adapters/ChooseCharacterAdapter.kt
+++ app/src/main/java/ru/storytellers/ui/adapters/ChooseCharacterAdapter.kt
@@ -1,4 +1,25 @@
 package ru.storytellers.ui.adapters
 
-class ChooseCharacterAdapter {
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+
+class ChooseCharacterAdapter:RecyclerView.Adapter<ChooseCharacterAdapter.ViewHolder>()  {
+
+
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+    inner class ViewHolder(container: View) : RecyclerView.ViewHolder(container){
+
+    }
 }
