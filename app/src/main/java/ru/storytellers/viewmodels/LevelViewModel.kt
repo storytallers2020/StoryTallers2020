55c9dfcaea68117256cb78f4192a4d7b00b1563c
Vitas1968 <dor1968@mail.ru>
2020-07-03 18:19:55 +0700

    добавил свойство levelGame и метод для получения из фрагмента выбранного уровня игры

--- app/src/main/java/ru/storytellers/viewmodels/LevelViewModel.kt
+++ app/src/main/java/ru/storytellers/viewmodels/LevelViewModel.kt
@@ -4,12 +4,18 @@ package ru.storytellers.viewmodels
 import androidx.lifecycle.LiveData
 import ru.storytellers.viewmodels.baseviewmodel.BaseViewModel
 import ru.storytellers.model.DataModel
+import kotlin.properties.Delegates
 
 class LevelViewModel : BaseViewModel<DataModel>() {
     private val liveDataForViewToObserve: LiveData<DataModel> = _mutableLiveData
+    private var levelGame:Int=0
+
 
     override fun subscribe(): LiveData<DataModel> {
         return liveDataForViewToObserve
+    }
+    fun setLevelGame(levelGame:Int){
+        this.levelGame=levelGame
     }
 }
 
