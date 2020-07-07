361b1df93fafdf35b7faad6b307e695e5a8061c2
Vitas1968 <dor1968@mail.ru>
2020-07-07 22:50:25 +0700

    эксперементировал, понадобился контекст. СОздал компаньон для получения экземляра приложения

--- app/src/main/java/ru/storytellers/application/StoryTallerApp.kt
+++ app/src/main/java/ru/storytellers/application/StoryTallerApp.kt
@@ -6,8 +6,15 @@ import org.koin.core.context.startKoin
 import timber.log.Timber
 
 class StoryTallerApp: Application() {
+
+    companion object {
+        private lateinit var application: StoryTallerApp
+        fun getInstanceApp() = application
+    }
+
     override fun onCreate() {
         super.onCreate()
+        application=this
         startKoin { androidContext(this@StoryTallerApp) }
         Timber.plant(Timber.DebugTree())
     }
