56bc0392bd1411deb36ece0a2a71cc3986076745
Vitas1968 <dor1968@mail.ru>
2020-07-07 22:45:12 +0700

    реализация модели

--- app/src/main/java/ru/storytellers/viewmodels/LevelViewModel.kt
+++ app/src/main/java/ru/storytellers/viewmodels/LevelViewModel.kt
@@ -2,20 +2,28 @@ package ru.storytellers.viewmodels
 
 
 import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import ru.storytellers.viewmodels.baseviewmodel.BaseViewModel
 import ru.storytellers.model.DataModel
 import kotlin.properties.Delegates
 
 class LevelViewModel : BaseViewModel<DataModel>() {
-    private val liveDataForViewToObserve: LiveData<DataModel> = _mutableLiveData
+    private val levelGameLiveData= MutableLiveData<Int>()
     private var levelGame:Int=0
 
 
-    override fun subscribe(): LiveData<DataModel> {
-        return liveDataForViewToObserve
+
+    fun subscribeOnLevelGame():LiveData<Int>{
+        return levelGameLiveData
     }
+
     fun setLevelGame(levelGame:Int){
         this.levelGame=levelGame
+        levelGameLiveData.value=this.levelGame
+    }
+
+    override fun subscribe(): LiveData<DataModel> {
+        TODO("Not yet implemented")
     }
 }
 
