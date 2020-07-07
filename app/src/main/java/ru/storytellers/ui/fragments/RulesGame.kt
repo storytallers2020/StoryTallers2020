8bc1c4b9939923503c203301e37d6cace77cc82a
vasic2000 <vasic2000@yandex.ru>
2020-07-07 22:49:25 +0300

    Смерджил локально remout/master в свою перед пул реквестом.

--- app/src/main/java/ru/storytellers/ui/fragments/RulesGame.kt
+++ app/src/main/java/ru/storytellers/ui/fragments/RulesGame.kt
@@ -5,6 +5,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import kotlinx.android.synthetic.main.fragment_rules.*
 import org.koin.android.ext.android.inject
 import ru.storytellers.R
 import ru.storytellers.navigation.Screens
