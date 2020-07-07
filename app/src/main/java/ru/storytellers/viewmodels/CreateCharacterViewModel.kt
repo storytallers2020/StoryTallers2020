919084d07049d4249ac1c2546ce7cc36526f40dd
Vitas1968 <dor1968@mail.ru>
2020-07-07 22:18:54 +0700

    реализвция модели с отдельными LiveDate для ошибок и успешного результата

--- app/src/main/java/ru/storytellers/viewmodels/CreateCharacterViewModel.kt
+++ app/src/main/java/ru/storytellers/viewmodels/CreateCharacterViewModel.kt
@@ -1,15 +1,51 @@
 package ru.storytellers.viewmodels
 
 
+import android.view.View
 import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers
+import ru.storytellers.application.StoryTallerApp
 import ru.storytellers.viewmodels.baseviewmodel.BaseViewModel
 import ru.storytellers.model.DataModel
+import ru.storytellers.model.entity.Character
+import ru.storytellers.model.repository.CharacterRepository
+import ru.storytellers.model.repository.ICharacterRepository
 
-class CreateCharacterViewModel : BaseViewModel<DataModel>() {
-    private val liveDataForViewToObserve: LiveData<DataModel> = _mutableLiveData
-    override fun subscribe(): LiveData<DataModel> {
-        return liveDataForViewToObserve
+class CreateCharacterViewModel(private val characterRepository: ICharacterRepository) : BaseViewModel<DataModel>() {
+
+    private val onSuccessliveData = MutableLiveData<DataModel.Success<Character>>()
+    private val onErrorliveData = MutableLiveData<DataModel.Error>()
+    private val onLoadingliveData = MutableLiveData<DataModel.Loading>()
+
+
+     fun subscribeOnSuccess(): LiveData<DataModel.Success<Character>>{
+        return onSuccessliveData
+     }
+    fun subscribeOnError(): LiveData<DataModel.Error>{
+        return onErrorliveData
     }
+    fun subscribeOnLoading(): LiveData<DataModel.Loading>{
+        return onLoadingliveData
+    }
+
+    fun getAllCharacters(){
+        characterRepository.getAll()
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribe({
+                onSuccessliveData.value=DataModel.Success(it)
+            },{
+                onErrorliveData.value=DataModel.Error(it)
+            })
+
+
+
+    }
+
+    override fun subscribe(): LiveData<DataModel> {
+        TODO("Not yet implemented")
+    }
+
 }
 
 
