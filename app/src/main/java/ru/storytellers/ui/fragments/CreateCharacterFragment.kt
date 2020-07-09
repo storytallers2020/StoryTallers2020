3e399b582067c08a7df4a981fd36fb023de0add1
Vitas1968 <dor1968@mail.ru>
2020-07-10 00:13:03 +0700

    нарисовал и обработал три поля ввода имен игроков

--- app/src/main/java/ru/storytellers/ui/fragments/CreateCharacterFragment.kt
+++ app/src/main/java/ru/storytellers/ui/fragments/CreateCharacterFragment.kt
@@ -1,15 +1,18 @@
 package ru.storytellers.ui.fragments
 
-import android.view.View
+import android.text.Editable
+import android.text.TextWatcher
 import androidx.lifecycle.Observer
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
-import kotlinx.android.synthetic.main.fragment_character_create.*
+import com.google.android.material.textfield.TextInputEditText
+import kotlinx.android.synthetic.main.fragment_character_create.back_button_character
+import kotlinx.android.synthetic.main.fragment_character_create.btn_next
+import kotlinx.android.synthetic.main.fragment_character_create_v2.*
 import org.koin.android.scope.currentScope
 import ru.storytellers.R
 import ru.storytellers.model.DataModel
-import ru.storytellers.model.entity.Character
 import ru.storytellers.navigation.Screens
 import ru.storytellers.ui.adapters.ChooseCharacterAdapter
 import ru.storytellers.ui.fragments.basefragment.BaseFragment
@@ -21,7 +24,21 @@ class CreateCharacterFragment(private val levelGame:Int): BaseFragment<DataModel
     override lateinit var model: CreateCharacterViewModel
     private val characterRecyclerView by viewById<RecyclerView>(R.id.rv_characters)
     private val characterAdapter: ChooseCharacterAdapter by lazy { ChooseCharacterAdapter() }
-    override val layoutRes= R.layout.fragment_character_create
+    override val layoutRes= R.layout.fragment_character_create_v2
+    private var inputNameGamer: String?=null
+    private var nameGamer1: String?=null
+    private var nameGamer2: String?=null
+    private var nameGamer3: String?=null
+    private val textWatcher = object : TextWatcher {
+        override fun onTextChanged(s: CharSequence, start: Int, before: Int, count: Int) {
+
+        }
+        override fun beforeTextChanged(s: CharSequence, start: Int, count: Int, after: Int) {}
+        override fun afterTextChanged(s: Editable) {
+            inputNameGamer=s.toString()
+
+        }
+    }
 
     companion object {
         fun newInstance(levelGame:Int) = CreateCharacterFragment(levelGame)
@@ -59,14 +76,52 @@ class CreateCharacterFragment(private val levelGame:Int): BaseFragment<DataModel
     }
 
     override fun init() {
+        input_name_field1_et.tag="namePlayer1"
+        input_name_field2_et.tag = "namePlayer2"
+        input_name_field3_et.tag = "namePlayer3"
+        setTextChangeListener(input_name_field1_et)
+        setTextChangeListener(input_name_field2_et)
+        setTextChangeListener(input_name_field3_et)
         iniViewModel()
         btn_next.setOnClickListener { navigateToLocationScreen() }
         back_button_character.setOnClickListener {backClicked()}
         initRecycler()
     }
 
+    private fun setTextChangeListener(inputNameField:TextInputEditText){
+        inputNameField.setOnFocusChangeListener { view, hasFocus ->
+           view.run { this as? TextInputEditText }
+               ?.let { textInputEditText->
+                   if (hasFocus) textInputEditText.addTextChangedListener(textWatcher)
+                   else {
+                       selectionOfEnteredValue(textInputEditText)
+                           textInputEditText.removeTextChangedListener(textWatcher)}
+               }
+        }
+    }
+
+    private fun selectionOfEnteredValue(textInputEditText: TextInputEditText) {
+        when (textInputEditText.tag) {
+            "namePlayer1" -> {
+                inputNameGamer?.let {
+                    nameGamer1 = it
+                }
+            }
+            "namePlayer2" -> {
+                inputNameGamer?.let {
+                    nameGamer2 = it
+                }
+            }
+            else -> {
+                inputNameGamer?.let {
+                    nameGamer3 = it
+                }
+            }
+        }
+    }
+
     private fun navigateToLocationScreen() {
-        router.navigateTo(Screens.LocationlScreen())
+        router.navigateTo(Screens.LocationScreen())
     }
 
     private fun initRecycler(){
