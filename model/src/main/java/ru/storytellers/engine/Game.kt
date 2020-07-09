8744cef371d89685c8e6891e9f55c07c3f9a6168
LeonovEgor <leonov.egor@gmail.com>
2020-07-09 09:09:20 +0300

    Исправлена ошибка возврата игрока. Рефакторинг на основе замечаний с урока.

--- model/src/main/java/ru/storytellers/engine/Game.kt
+++ model/src/main/java/ru/storytellers/engine/Game.kt
@@ -3,19 +3,21 @@ package ru.storytellers.engine
 import ru.storytellers.engine.level.Level
 import ru.storytellers.model.entity.Character
 import ru.storytellers.model.entity.SentenceOfTale
+import ru.storytellers.utils.getPlayerNumByTurn
 
 class Game() {
 
     lateinit var level: Level
     private lateinit var players: List<Character>
-    private var turn: Int = 0
+    private var turn: Int = 1
 
-    fun getCurrentPlayer(): Character =
-        if (turn <= players.count()) players[turn - 1]
-        else players[turn - turn / players.count()]
+    fun getCurrentPlayer(): Character {
+        val playerNum = getPlayerNumByTurn(turn, players.count())
+        return players[playerNum-1]
+    }
 
     fun newGame(players: List<Character>, level: Level) {
-        turn = 0
+        turn = 1
 
         this.players = players
         this.level = level
@@ -26,11 +28,9 @@ class Game() {
             .rules
             .isSentenceCorrect(sentenceOfTale.content)
 
-        return if (res) {
-            turn++
-            true
-        } else
-            false
+        if (res) turn++
+
+        return res
     }
 
 }
