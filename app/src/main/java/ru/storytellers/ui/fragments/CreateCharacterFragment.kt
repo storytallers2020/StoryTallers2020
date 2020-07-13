6cc1d5de3461c2f34e4bf75746b0a60b4072a6c8
Vitas1968 <dor1968@mail.ru>
2020-07-13 23:51:25 +0700

    добавил рециклер для игровов, адаптер для него и обработку ввода текста в поле ввода имени игрока

--- app/src/main/java/ru/storytellers/ui/fragments/CreateCharacterFragment.kt
+++ app/src/main/java/ru/storytellers/ui/fragments/CreateCharacterFragment.kt
@@ -1,7 +1,6 @@
 package ru.storytellers.ui.fragments
 
-import android.text.Editable
-import android.text.TextWatcher
+import android.view.inputmethod.EditorInfo
 import androidx.lifecycle.Observer
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -9,12 +8,13 @@ import androidx.recyclerview.widget.RecyclerView
 import com.google.android.material.textfield.TextInputEditText
 import kotlinx.android.synthetic.main.fragment_character_create.back_button_character
 import kotlinx.android.synthetic.main.fragment_character_create.btn_next
-import kotlinx.android.synthetic.main.fragment_character_create_v2.*
+import kotlinx.android.synthetic.main.fragment_character_create_v3.*
 import org.koin.android.scope.currentScope
 import ru.storytellers.R
 import ru.storytellers.model.DataModel
 import ru.storytellers.navigation.Screens
 import ru.storytellers.ui.adapters.ChooseCharacterAdapter
+import ru.storytellers.ui.adapters.PlayerAdapter
 import ru.storytellers.ui.fragments.basefragment.BaseFragment
 import ru.storytellers.utils.viewById
 import ru.storytellers.viewmodels.CreateCharacterViewModel
@@ -23,22 +23,10 @@ import timber.log.Timber
 class CreateCharacterFragment(private val levelGame:Int): BaseFragment<DataModel>() {
     override lateinit var model: CreateCharacterViewModel
     private val characterRecyclerView by viewById<RecyclerView>(R.id.rv_characters)
+    private val playerRecyclerView by viewById<RecyclerView>(R.id.player_list_rv)
     private val characterAdapter: ChooseCharacterAdapter by lazy { ChooseCharacterAdapter() }
-    override val layoutRes= R.layout.fragment_character_create_v2
-    private var inputNameGamer: String?=null
-    private var nameGamer1: String?=null
-    private var nameGamer2: String?=null
-    private var nameGamer3: String?=null
-    private val textWatcher = object : TextWatcher {
-        override fun onTextChanged(s: CharSequence, start: Int, before: Int, count: Int) {
-
-        }
-        override fun beforeTextChanged(s: CharSequence, start: Int, count: Int, after: Int) {}
-        override fun afterTextChanged(s: Editable) {
-            inputNameGamer=s.toString()
-
-        }
-    }
+    private val playerAdapter: PlayerAdapter by lazy { PlayerAdapter() }
+    override val layoutRes= R.layout.fragment_character_create_v3
 
     companion object {
         fun newInstance(levelGame:Int) = CreateCharacterFragment(levelGame)
@@ -56,6 +44,7 @@ class CreateCharacterFragment(private val levelGame:Int): BaseFragment<DataModel
             getAllCharacters()
             handlerOnSuccessResult(this)
             handlerOnErrorResult(this)
+            handlerNamePlayerList(this)
         }
     }
 
@@ -74,66 +63,62 @@ class CreateCharacterFragment(private val levelGame:Int): BaseFragment<DataModel
             }
         })
     }
+    private fun handlerNamePlayerList(viewModel:CreateCharacterViewModel) {
+        viewModel.subscribePlayersLiveData().observe(viewLifecycleOwner, Observer {
+            it.let {
+                if (it.isNotEmpty()) {
+                    playerAdapter.setDataToPlayerAdapter(it)
+                }
+            }
+        })
+    }
 
     override fun init() {
-        input_name_field1_et.tag="namePlayer1"
-        input_name_field2_et.tag = "namePlayer2"
-        input_name_field3_et.tag = "namePlayer3"
-        setTextChangeListener(input_name_field1_et)
-        setTextChangeListener(input_name_field2_et)
-        setTextChangeListener(input_name_field3_et)
         iniViewModel()
+        setOnEditorActionListener(enter_name_field_et)
         btn_next.setOnClickListener { navigateToLocationScreen() }
         back_button_character.setOnClickListener {backClicked()}
-        initRecycler()
+        initRecyclers()
     }
 
-    private fun setTextChangeListener(inputNameField:TextInputEditText){
-        inputNameField.setOnFocusChangeListener { view, hasFocus ->
-           view.run { this as? TextInputEditText }
-               ?.let { textInputEditText->
-                   if (hasFocus) textInputEditText.addTextChangedListener(textWatcher)
-                   else {
-                       selectionOfEnteredValue(textInputEditText)
-                           textInputEditText.removeTextChangedListener(textWatcher)}
-               }
+    private fun setOnEditorActionListener(editTextView: TextInputEditText){
+        editTextView.setOnEditorActionListener { v, actionId, event ->
+            if(actionId == EditorInfo.IME_ACTION_DONE)
+            {
+                model.addNamePlayerToList(editTextView.text.toString())
+                editTextView.setText("")
+               // editTextView.isEnabled=false
+
+                return@setOnEditorActionListener true
+            }
+            return@setOnEditorActionListener false
         }
     }
 
-    private fun selectionOfEnteredValue(textInputEditText: TextInputEditText) {
-        when (textInputEditText.tag) {
-            "namePlayer1" -> {
-                inputNameGamer?.let {
-                    nameGamer1 = it
-                }
-            }
-            "namePlayer2" -> {
-                inputNameGamer?.let {
-                    nameGamer2 = it
-                }
-            }
-            else -> {
-                inputNameGamer?.let {
-                    nameGamer3 = it
-                }
-            }
-        }
-    }
 
     private fun navigateToLocationScreen() {
         router.navigateTo(Screens.LocationScreen())
     }
 
-    private fun initRecycler(){
+    private fun initRecyclers(){
        val gridLayoutManager = GridLayoutManager(
            context,
            2,
            LinearLayoutManager.HORIZONTAL,
-           true
+           false
            )
         characterRecyclerView.apply {
             layoutManager=gridLayoutManager
             adapter=characterAdapter
+        }
+        val linearLayoutManager=LinearLayoutManager(
+            context,
+            LinearLayoutManager.VERTICAL,
+            false
+        )
+        playerRecyclerView.apply {
+            layoutManager=linearLayoutManager
+            adapter=playerAdapter
         }
     }
 }
