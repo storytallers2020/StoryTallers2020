e280d84384499c280d99bad8e9e219c167d49933
Vitas1968 <dor1968@mail.ru>
2020-07-15 14:53:15 +0700

    вспомогательный класс для создания игрока

--- app/src/main/java/ru/storytellers/utils/PlayerCreator.kt
+++ app/src/main/java/ru/storytellers/utils/PlayerCreator.kt
@@ -1,4 +1,24 @@
 package ru.storytellers.utils
 
+import ru.storytellers.model.entity.Character
+import ru.storytellers.model.entity.Player
+
 class PlayerCreator {
+
+    private var idPlayer: Long=0
+    private lateinit var namePlayer: String
+    private lateinit var characterOfPlayer: Character
+
+    fun createPlayer(): Player {
+        idPlayer++
+        return Player(idPlayer,namePlayer,characterOfPlayer)
+    }
+
+    fun setNamePlayer(name: String){
+        namePlayer=name
+    }
+
+    fun setCharacterOfPlayer(character: Character){
+        characterOfPlayer=character
+    }
 }
