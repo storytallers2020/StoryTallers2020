871163bd76817b5d9cbe4f4b5815688566e2ab6c
Vitas1968 <dor1968@mail.ru>
2020-07-19 12:10:00 +0700

    чистка и оптимизация кода после удаления ненужного в базовой модели

--- app/src/main/java/ru/storytellers/viewmodels/StartViewModel.kt
+++ app/src/main/java/ru/storytellers/viewmodels/StartViewModel.kt
@@ -1,18 +1,10 @@
 package ru.storytellers.viewmodels
 
 
-import androidx.lifecycle.LiveData
 import ru.storytellers.viewmodels.baseviewmodel.BaseViewModel
 import ru.storytellers.model.DataModel
 
 class StartViewModel : BaseViewModel<DataModel>() {
-    private val liveDataForViewToObserve: LiveData<DataModel> = _mutableLiveData
-
-    override fun subscribe(): LiveData<DataModel> {
-        return liveDataForViewToObserve
-    }
-
-
 }
 
 
