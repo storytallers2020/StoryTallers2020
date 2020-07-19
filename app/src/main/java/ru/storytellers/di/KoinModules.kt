4d62d66dc68ff7b55dbbad72f0d67721676b36ad
Vitas1968 <dor1968@mail.ru>
2020-07-19 12:06:17 +0700

    создание перенес во фрагмент

--- app/src/main/java/ru/storytellers/di/KoinModules.kt
+++ app/src/main/java/ru/storytellers/di/KoinModules.kt
@@ -55,7 +55,6 @@ val levelModel =  module {
 }
 val characterModel =  module {
     single { PlayerCreator() }
-    single { PlayerAdapter() }
     single<ICharacterDataSource>{CharacterResDataSource(get()) }
     single<ICharacterRepository>{CharacterRepository(get()) }
     viewModel { CreateCharacterViewModel(get()) }
