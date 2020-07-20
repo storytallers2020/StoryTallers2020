aba58ffd80fe7637ff09b61904566cfe2c845f21
Vitas1968 <dor1968@mail.ru>
2020-07-21 00:53:19 +0700

    уровень игры не передаем

--- app/src/main/java/ru/storytellers/navigation/Screens.kt
+++ app/src/main/java/ru/storytellers/navigation/Screens.kt
@@ -8,8 +8,8 @@ sealed class Screens {
         override fun getFragment() = StartFragment.newInstance()
     }
 
-    class CreateCharacterScreen( private val levelGame:Int) : SupportAppScreen(){
-        override fun getFragment() = CreateCharacterFragment.newInstance(levelGame)
+    class CreateCharacterScreen() : SupportAppScreen(){
+        override fun getFragment() = CreateCharacterFragment.newInstance()
     }
 
     class LevelScreen : SupportAppScreen(){
