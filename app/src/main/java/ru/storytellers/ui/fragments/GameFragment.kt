8747709e3f4cf9e709c8472e6137aa85c42e632c
Vitas1968 <dor1968@mail.ru>
2020-07-21 23:10:08 +0700

    инициализация игры и функция навигации

--- app/src/main/java/ru/storytellers/ui/fragments/GameFragment.kt
+++ app/src/main/java/ru/storytellers/ui/fragments/GameFragment.kt
@@ -2,21 +2,15 @@ package ru.storytellers.ui.fragments
 
 import org.koin.android.ext.android.inject
 import ru.storytellers.R
-import ru.storytellers.application.StoryTallerApp
-import ru.storytellers.engine.Game
-import ru.storytellers.engine.level.Level
 import ru.storytellers.model.DataModel
-import ru.storytellers.model.entity.Player
 import ru.storytellers.navigation.Screens
 import ru.storytellers.ui.assistant.GameFragmentAssistant
 import ru.storytellers.ui.fragments.basefragment.BaseFragment
 import ru.storytellers.viewmodels.GameViewModel
 
 class GameFragment: BaseFragment<DataModel>() {
-    private val game: Game by inject()
-    private val level: Level by inject()
+
     private lateinit var assistantFragment: GameFragmentAssistant
-    private  val listPlayer= StoryTallerApp.instance.gameStorage.getListPlayers()
     override val model: GameViewModel by inject()
     override val layoutRes = R.layout.fragment_game
 
@@ -34,7 +28,7 @@ class GameFragment: BaseFragment<DataModel>() {
 
     override fun init() {
         assistantFragment=GameFragmentAssistant(this)
-        showHintAndHideGameScreen()
+        model.createNewGame()
         showIntro()
         setEndClickListener()
     }
@@ -45,6 +39,10 @@ class GameFragment: BaseFragment<DataModel>() {
 //        button_end.setOnClickListener {
 //            router.navigateTo(Screens.GameEndScreen())
 //        }
+    }
+
+    private fun navigateToGameEndScreen() {
+        router.navigateTo(Screens.GameEndScreen())
     }
 
     private fun showIntro() {
