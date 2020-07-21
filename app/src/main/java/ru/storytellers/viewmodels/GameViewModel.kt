6a91d1ca889423b0c1e27a712c66df376bcee66e
Vitas1968 <dor1968@mail.ru>
2020-07-21 21:03:25 +0700

    инициализация игры

--- app/src/main/java/ru/storytellers/viewmodels/GameViewModel.kt
+++ app/src/main/java/ru/storytellers/viewmodels/GameViewModel.kt
@@ -1,7 +1,21 @@
 package ru.storytellers.viewmodels
 
+import ru.storytellers.application.StoryTallerApp
+import ru.storytellers.engine.Game
+import ru.storytellers.engine.level.Levels
 import ru.storytellers.model.DataModel
 import ru.storytellers.viewmodels.baseviewmodel.BaseViewModel
 
-class GameViewModel: BaseViewModel<DataModel>() {
+class GameViewModel(
+    private val game: Game,
+    levels: Levels
+
+): BaseViewModel<DataModel>() {
+    private  val listPlayer= StoryTallerApp.instance.gameStorage.getListPlayers()
+    private  val levelGame= levels.getLevelById(StoryTallerApp.instance.gameStorage.getLevelGame())
+
+    fun createNewGame(){
+        levelGame?.let { game.newGame(listPlayer, it ) }
+    }
+
 }
