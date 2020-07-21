43147eda9d3f0b270d2f8592a26a8e3f43785454
Vitas1968 <dor1968@mail.ru>
2020-07-21 20:59:40 +0700

    фрагмент начала игры реализация

--- app/src/main/java/ru/storytellers/ui/fragments/GameStartFragment.kt
+++ app/src/main/java/ru/storytellers/ui/fragments/GameStartFragment.kt
@@ -1,4 +1,35 @@
 package ru.storytellers.ui.fragments
 
-class GameStartFragment {
+import kotlinx.android.synthetic.main.fragment_game_start.*
+import ru.storytellers.R
+import ru.storytellers.model.DataModel
+import ru.storytellers.navigation.Screens
+import ru.storytellers.ui.fragments.basefragment.BaseFragment
+import ru.storytellers.viewmodels.baseviewmodel.BaseViewModel
+
+class GameStartFragment: BaseFragment<DataModel>() {
+    override val model: BaseViewModel<DataModel>
+        get() = TODO("Not yet implemented")
+    override val layoutRes=R.layout.fragment_game_start
+    companion object {
+        fun newInstance() = GameStartFragment()
+    }
+
+    override fun init() {
+        btn_next.setOnClickListener { navigateToGameScreen() }
+        back_button_location.setOnClickListener { backClicked() }
+    }
+
+    override fun iniViewModel() {
+        TODO("Not yet implemented")
+    }
+
+    private fun navigateToGameScreen() {
+        router.navigateTo(Screens.GameScreen())
+    }
+
+    override fun backClicked(): Boolean {
+        router.exit()
+        return true
+    }
 }
