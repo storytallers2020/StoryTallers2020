26cfe6e0701e96fd87c1ce8867d5e38d6f3e4839
Vitas1968 <dor1968@mail.ru>
2020-07-27 21:32:30 +0700

    тоже пока пустой

--- app/src/main/java/ru/storytellers/ui/fragments/SelectCoverFragment.kt
+++ app/src/main/java/ru/storytellers/ui/fragments/SelectCoverFragment.kt
@@ -1,4 +1,28 @@
 package ru.storytellers.ui.fragments
 
-class SelectCoverFragment {
+import org.koin.android.ext.android.inject
+import ru.storytellers.R
+import ru.storytellers.model.DataModel
+import ru.storytellers.ui.fragments.basefragment.BaseFragment
+import ru.storytellers.viewmodels.SelectCoverViewModel
+
+class SelectCoverFragment: BaseFragment<DataModel>() {
+    override val model: SelectCoverViewModel by inject()
+    override val layoutRes= R.layout.fragment_choosing_cover
+    companion object {
+        fun newInstance() = SelectCoverFragment()
+    }
+
+
+    override fun init() {
+        TODO("Not yet implemented")
+    }
+
+    override fun iniViewModel() {
+        TODO("Not yet implemented")
+    }
+
+    override fun backClicked(): Boolean {
+        TODO("Not yet implemented")
+    }
 }
