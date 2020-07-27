5b81a59f502cf18f036ab4b401d2411befb6a9ec
Vitas1968 <dor1968@mail.ru>
2020-07-27 21:36:40 +0700

    добавил функции форматирования текста(не работают)

--- app/src/main/java/ru/storytellers/viewmodels/GameViewModel.kt
+++ app/src/main/java/ru/storytellers/viewmodels/GameViewModel.kt
@@ -10,22 +10,24 @@ import ru.storytellers.model.entity.ContentTypeEnum
 import ru.storytellers.model.entity.Player
 import ru.storytellers.model.entity.SentenceOfTale
 import ru.storytellers.ui.assistant.GameViewModelAssistant
+import ru.storytellers.utils.addDotIfNeed
+import ru.storytellers.utils.collectSentence
 import ru.storytellers.utils.getUid
+import ru.storytellers.utils.trimSentenceSpace
 import ru.storytellers.viewmodels.baseviewmodel.BaseViewModel
 
 class GameViewModel(
     private val game: Game,
-    private val viewModelAssistant: GameViewModelAssistant,
     levels: Levels
 
 ): BaseViewModel<DataModel>() {
     private  val listPlayer= StoryTallerApp.instance.gameStorage.getListPlayers()
     private  val listSentenceOfTale= StoryTallerApp.instance.gameStorage.getListSentenceOfTale()
     private  val levelGame= levels.getLevelById(StoryTallerApp.instance.gameStorage.getLevelGame())
-    private var stringBuilder=StringBuilder()
     private var currentPlayer: Player?=null
     var isCorrectFlag: Boolean=true
     private var currentSentenceOfTale: SentenceOfTale?=null
+    private val textResult= mutableListOf<String>()
 
     private val currentPlayerLiveData = MutableLiveData<Player>()
     private val resultTextLiveData = MutableLiveData<String>()
@@ -45,6 +47,11 @@ class GameViewModel(
     fun subscribeOnIsCorrectFlag()=isCorrectFlagLiveData
 
     fun createSentenceOfTale(content:String) {
+        content.apply {
+            trimSentenceSpace()
+            addDotIfNeed()
+        }
+
         currentPlayer?.let {
             SentenceOfTale(
                 getUid(),
@@ -71,10 +78,12 @@ class GameViewModel(
     }
 
     private fun handlerCurrentSentenceOfTale(){
-        listSentenceOfTale.add(currentSentenceOfTale)
-        stringBuilder.append(currentSentenceOfTale?.content).append(" ")
-        if (stringBuilder.isNotEmpty()) {
-            resultTextLiveData.value = stringBuilder.toString()
+        listSentenceOfTale.apply {
+            add(currentSentenceOfTale)
+            textResult.clear()
+            forEach {textResult.add(it!!.content)}
         }
+        resultTextLiveData.value = textResult.collectSentence()
+
     }
 }
