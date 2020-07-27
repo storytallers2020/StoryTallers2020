1be5b4ca0118aa2f7596e13f320f606ff2e422c6
Vitas1968 <dor1968@mail.ru>
2020-07-27 21:39:16 +0700

    реализация фрагмента отображения сказки

--- app/src/main/java/ru/storytellers/ui/fragments/GameEndFragment.kt
+++ app/src/main/java/ru/storytellers/ui/fragments/GameEndFragment.kt
@@ -1,26 +1,35 @@
 package ru.storytellers.ui.fragments
 
+import androidx.lifecycle.Observer
+import kotlinx.android.synthetic.main.fragment_game.*
 import kotlinx.android.synthetic.main.fragment_game_end.*
 import org.koin.android.ext.android.inject
 import ru.storytellers.R
 import ru.storytellers.model.DataModel
 import ru.storytellers.navigation.Screens
 import ru.storytellers.ui.fragments.basefragment.BaseFragment
+import ru.storytellers.utils.loadImage
+import ru.storytellers.utils.resourceToUri
 import ru.storytellers.viewmodels.GameEndViewModel
 
-class GameEndFragment: BaseFragment<DataModel>() {
+class GameEndFragment(
+    private val textResultStoryTaller:String
+): BaseFragment<DataModel>() {
     override  val model: GameEndViewModel by inject()
     override val layoutRes = R.layout.fragment_game_end
 
     companion object {
-        fun newInstance() = GameEndFragment()
+        fun newInstance(textResultStoryTaller:String) = GameEndFragment(textResultStoryTaller)
     }
 
     override fun iniViewModel() {
     }
 
     override fun init() {
-        setResumeClickListener()
+        model.setTextOfStoryTaller(textResultStoryTaller)
+        handlerTextOfStoryTaller()
+        btn_select_cover.setOnClickListener { navigateToSelectCoverScreen() }
+        //   setResumeClickListener()
     }
 
     fun setResumeClickListener() {
@@ -28,9 +37,17 @@ class GameEndFragment: BaseFragment<DataModel>() {
             router.navigateTo(Screens.GameScreen())
         }
     }
+    private fun handlerTextOfStoryTaller(){
+        model.subscribeOnTextOfStoryTaller().observe(viewLifecycleOwner, Observer {
+            tv_tale.text=it
+        })
+    }
 
     override fun backClicked(): Boolean {
         router.exit()
         return true
+    }
+    private fun navigateToSelectCoverScreen() {
+         router.navigateTo(Screens.SelectCoverScreen())
     }
 }
