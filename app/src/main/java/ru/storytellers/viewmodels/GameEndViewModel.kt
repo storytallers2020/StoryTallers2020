d78d23839f4becf3d2f41c66203066e261193ffc
Vitas1968 <dor1968@mail.ru>
2020-07-27 21:38:50 +0700

    реализация модели отображения сказки

--- app/src/main/java/ru/storytellers/viewmodels/GameEndViewModel.kt
+++ app/src/main/java/ru/storytellers/viewmodels/GameEndViewModel.kt
@@ -1,7 +1,14 @@
 package ru.storytellers.viewmodels
 
+import androidx.lifecycle.MutableLiveData
 import ru.storytellers.model.DataModel
 import ru.storytellers.viewmodels.baseviewmodel.BaseViewModel
 
 class GameEndViewModel: BaseViewModel<DataModel>() {
+    private val textOfStoryTallerLiveData = MutableLiveData<String>()
+
+    fun subscribeOnTextOfStoryTaller()=textOfStoryTallerLiveData
+    fun setTextOfStoryTaller(textOfStoryTaller:String){
+        textOfStoryTallerLiveData.value=textOfStoryTaller
+    }
 }
