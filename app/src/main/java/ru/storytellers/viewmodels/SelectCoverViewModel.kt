5f68f6a6e875afcd853fabfec1b7089c8c35c709
Vitas1968 <dor1968@mail.ru>
2020-07-28 21:47:18 +0700

    модель выбора обложки

--- app/src/main/java/ru/storytellers/viewmodels/SelectCoverViewModel.kt
+++ app/src/main/java/ru/storytellers/viewmodels/SelectCoverViewModel.kt
@@ -1,7 +1,34 @@
 package ru.storytellers.viewmodels
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers
 import ru.storytellers.model.DataModel
+import ru.storytellers.model.entity.Location
+import ru.storytellers.model.repository.ILocationRepository
 import ru.storytellers.viewmodels.baseviewmodel.BaseViewModel
 
-class SelectCoverViewModel: BaseViewModel<DataModel>() {
+class SelectCoverViewModel(
+    private val locationRepository: ILocationRepository
+): BaseViewModel<DataModel>() {
+    private val onSuccessLiveData = MutableLiveData<DataModel.Success<Location>>()
+    private val onErrorLiveData = MutableLiveData<DataModel.Error>()
+
+    fun getAllCover() {
+        locationRepository.getAll()
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribe({
+                onSuccessLiveData.value = DataModel.Success(it)
+            }, {
+                onErrorLiveData.value = DataModel.Error(it)
+            })
+    }
+
+    fun subscribeOnSuccess(): LiveData<DataModel.Success<Location>> {
+        return onSuccessLiveData
+    }
+
+    fun subscribeOnError(): LiveData<DataModel.Error> {
+        return onErrorLiveData
+    }
 }
