4cceea168c0bd1284a1b8495bbd241f843f44211
Vitas1968 <dor1968@mail.ru>
2020-07-28 21:48:29 +0700

    модель ввода названия и сохранения сказки

--- app/src/main/java/ru/storytellers/viewmodels/TitleAndSaveStoryViewModel.kt
+++ app/src/main/java/ru/storytellers/viewmodels/TitleAndSaveStoryViewModel.kt
@@ -1,4 +1,27 @@
 package ru.storytellers.viewmodels
 
-class TitleAndSaveStoryViewModel {
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import ru.storytellers.application.StoryTallerApp
+import ru.storytellers.model.DataModel
+import ru.storytellers.model.entity.Location
+import ru.storytellers.ui.assistant.TitleAndSaveModelAssistant
+import ru.storytellers.viewmodels.baseviewmodel.BaseViewModel
+
+class TitleAndSaveStoryViewModel(
+    private val assistantModel:TitleAndSaveModelAssistant
+): BaseViewModel<DataModel>() {
+    private var titleStory:String?=null
+    private val coverLiveDate= MutableLiveData<Location>()
+
+    fun setTitleStory(title:String){
+        titleStory= title
+        StoryTallerApp.instance.gameStorage.setTitleStory(titleStory!!)
+        val asd="sdf"
+    }
+
+    fun subscribeOnCover():LiveData<Location>{
+        coverLiveDate.value=StoryTallerApp.instance.gameStorage.getCoverStoryTaller()
+        return coverLiveDate
+    }
 }
