57fd48df156f77321e1248269c01f2eee7db7135
Vitas1968 <dor1968@mail.ru>
2020-07-28 21:46:50 +0700

    фрагмент выбора обложки

--- app/src/main/java/ru/storytellers/ui/fragments/SelectCoverFragment.kt
+++ app/src/main/java/ru/storytellers/ui/fragments/SelectCoverFragment.kt
@@ -1,28 +1,71 @@
 package ru.storytellers.ui.fragments
 
+import androidx.lifecycle.Observer
+import kotlinx.android.synthetic.main.fragment_choosing_cover.*
 import org.koin.android.ext.android.inject
 import ru.storytellers.R
+import ru.storytellers.application.StoryTallerApp
 import ru.storytellers.model.DataModel
+import ru.storytellers.model.entity.Location
+import ru.storytellers.navigation.Screens
+import ru.storytellers.ui.adapters.CoverAdapter
 import ru.storytellers.ui.fragments.basefragment.BaseFragment
+import ru.storytellers.utils.fieldsToLogString
 import ru.storytellers.viewmodels.SelectCoverViewModel
+import timber.log.Timber
 
+// временно обложки изобрадают локации
 class SelectCoverFragment: BaseFragment<DataModel>() {
     override val model: SelectCoverViewModel by inject()
     override val layoutRes= R.layout.fragment_choosing_cover
     companion object {
         fun newInstance() = SelectCoverFragment()
     }
+    private val onListItemClickListener = object : CoverAdapter.OnListItemClickListener {
+        override fun onItemClick(cover: Location) {
+            StoryTallerApp.instance.gameStorage.setCoverStoryTaller(cover)
+            Timber.d(cover.fieldsToLogString())
+            router.navigateTo(Screens.TitleAndSaveStoryScreen())
+        }
+    }
+    private val coverAdapter: CoverAdapter by lazy { CoverAdapter(onListItemClickListener) }
 
 
     override fun init() {
-        TODO("Not yet implemented")
+        initRecycler()
+        iniViewModel()
     }
 
     override fun iniViewModel() {
-        TODO("Not yet implemented")
+        model.apply {
+            getAllCover()
+            handlerOnSuccessResult(this)
+            handlerOnErrorResult(this)
+        }
+    }
+    private fun initRecycler(){
+        rv_covers.adapter=coverAdapter
+    }
+    private fun handlerOnSuccessResult(viewModel: SelectCoverViewModel) {
+        viewModel.subscribeOnSuccess().observe(viewLifecycleOwner, Observer {
+            it.let {
+                setLocationAdapter(it)
+            }
+        })
+    }
+
+    private fun handlerOnErrorResult(viewModel: SelectCoverViewModel) {
+        viewModel.subscribeOnError().observe(viewLifecycleOwner, Observer {
+            Timber.e(it.error)
+        })
+    }
+
+    private fun setLocationAdapter(it: DataModel.Success<Location>) {
+        coverAdapter.setData(it.data)
     }
 
     override fun backClicked(): Boolean {
-        TODO("Not yet implemented")
+        router.exit()
+        return true
     }
 }
