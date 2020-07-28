6c256199eb3d24a472208e0adecd08cb92ed3955
Vitas1968 <dor1968@mail.ru>
2020-07-28 21:49:04 +0700

    ассистент модели экрана ввода названия и сохранения сказки

--- app/src/main/java/ru/storytellers/ui/assistant/TitleAndSaveModelAssistant.kt
+++ app/src/main/java/ru/storytellers/ui/assistant/TitleAndSaveModelAssistant.kt
@@ -1,10 +1,10 @@
 package ru.storytellers.ui.assistant
 
-import ru.storytellers.model.repository.SentenceOfTaleRepository
+import ru.storytellers.model.repository.IStoryRepository
 
 //класс для работы с базой данный и сервером
-class GameViewModelAssistant(
-    private val sentenceOfTaleRepository: SentenceOfTaleRepository
+class TitleAndSaveModelAssistant(
+    private val storyRepository: IStoryRepository
 ) {
 
 }
