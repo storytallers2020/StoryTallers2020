fe094d637e60390ec052cf9c02a1a311385d927b
Vitas1968 <dor1968@mail.ru>
2020-07-30 22:06:56 +0700

    сохранение в хранилище игры выбраной обложки

--- app/src/main/java/ru/storytellers/viewmodels/SelectCoverViewModel.kt
+++ app/src/main/java/ru/storytellers/viewmodels/SelectCoverViewModel.kt
@@ -3,11 +3,10 @@ package ru.storytellers.viewmodels
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers
+import ru.storytellers.application.StoryTallerApp
 import ru.storytellers.model.DataModel
 import ru.storytellers.model.entity.Cover
-import ru.storytellers.model.entity.Location
 import ru.storytellers.model.repository.ICoverRepository
-import ru.storytellers.model.repository.ILocationRepository
 import ru.storytellers.viewmodels.baseviewmodel.BaseViewModel
 
 class SelectCoverViewModel(
@@ -32,5 +31,9 @@ class SelectCoverViewModel(
 
     fun subscribeOnError(): LiveData<DataModel.Error> {
         return onErrorLiveData
+    }
+
+    fun setCoverStory(cover: Cover){
+        StoryTallerApp.instance.gameStorage.setCoverStoryTaller(cover)
     }
 }
