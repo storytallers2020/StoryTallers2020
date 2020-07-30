864459d11318bbbb8ea0f80983acfd42b8205fc7
Vitas1968 <dor1968@mail.ru>
2020-07-30 21:59:13 +0700

    получаем uri фоновой картинки и суем его в livedata

--- app/src/main/java/ru/storytellers/viewmodels/GameEndViewModel.kt
+++ app/src/main/java/ru/storytellers/viewmodels/GameEndViewModel.kt
@@ -1,14 +1,29 @@
 package ru.storytellers.viewmodels
 
+import android.net.Uri
 import androidx.lifecycle.MutableLiveData
+import ru.storytellers.application.StoryTallerApp
 import ru.storytellers.model.DataModel
+import ru.storytellers.utils.resourceToUri
 import ru.storytellers.viewmodels.baseviewmodel.BaseViewModel
 
 class GameEndViewModel: BaseViewModel<DataModel>() {
     private val textOfStoryTallerLiveData = MutableLiveData<String>()
+    private val uriBackgroundImageLiveData = MutableLiveData<Uri>()
+
 
     fun subscribeOnTextOfStoryTaller()=textOfStoryTallerLiveData
+    fun subscribeOnUriBackgroundImage()=uriBackgroundImageLiveData
     fun setTextOfStoryTaller(textOfStoryTaller:String){
         textOfStoryTallerLiveData.value=textOfStoryTaller
     }
+    fun getUriBackgroundImage(){
+        StoryTallerApp
+            .instance
+            .gameStorage
+            .getLocationGame()?.
+            imageUrl?.
+            let { uriBackgroundImageLiveData.value=resourceToUri(it) }
+    }
+
 }
