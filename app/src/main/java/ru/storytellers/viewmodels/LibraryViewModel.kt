897a470a6230699dfd037251966ce11fa27f363b
Vitas1968 <dor1968@mail.ru>
2020-08-03 23:34:58 +0700

    содал классы библиотеки сказок

---
+++ app/src/main/java/ru/storytellers/viewmodels/LibraryViewModel.kt
@@ -0,0 +1,7 @@
+package ru.storytellers.viewmodels
+
+import ru.storytellers.model.DataModel
+import ru.storytellers.viewmodels.baseviewmodel.BaseViewModel
+
+class LibraryVirwModel: BaseViewModel<DataModel>() {
+}
