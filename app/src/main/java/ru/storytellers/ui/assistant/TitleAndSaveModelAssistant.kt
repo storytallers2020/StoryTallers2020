dd957115e79d5fe30db300326387d1ff952b77eb
Vitas1968 <dor1968@mail.ru>
2020-08-03 21:14:39 +0700

    сохранение класса Story в БД. При удачном сохранении возвращаем true при неудачном false

--- app/src/main/java/ru/storytellers/ui/assistant/TitleAndSaveModelAssistant.kt
+++ app/src/main/java/ru/storytellers/ui/assistant/TitleAndSaveModelAssistant.kt
@@ -1,10 +1,24 @@
 package ru.storytellers.ui.assistant
 
+import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers
+import ru.storytellers.model.entity.Story
 import ru.storytellers.model.repository.IStoryRepository
 
 //класс для работы с базой данный и сервером
 class TitleAndSaveModelAssistant(
     private val storyRepository: IStoryRepository
 ) {
+
+    fun saveStory(story: Story):Boolean{
+        var successSaveFlag:Boolean=false
+        storyRepository.insertOrReplace(story)
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribe({
+                successSaveFlag=true
+            },{
+                successSaveFlag=false
+            })
+        return successSaveFlag
+    }
 
 }
