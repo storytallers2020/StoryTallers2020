a4f7c4a0b3cf0cb24489a0864f94a0458333791c
Vitas1968 <dor1968@mail.ru>
2020-08-04 19:18:01 +0700

    реализация экрана отображения сказки из библиотеки

--- app/src/main/java/ru/storytellers/ui/fragments/LibraryBookFragment.kt
+++ app/src/main/java/ru/storytellers/ui/fragments/LibraryBookFragment.kt
@@ -1,4 +1,40 @@
 package ru.storytellers.ui.fragments
 
-class LibraryBookFragment {
+import androidx.lifecycle.Observer
+import kotlinx.android.synthetic.main.fragment_library_book.*
+import org.koin.android.ext.android.inject
+import ru.storytellers.R
+import ru.storytellers.model.DataModel
+import ru.storytellers.model.entity.Story
+import ru.storytellers.ui.fragments.basefragment.BaseFragment
+import ru.storytellers.viewmodels.LibraryBookViewModel
+
+class LibraryBookFragment(private val story: Story):BaseFragment<DataModel>() {
+    override val model: LibraryBookViewModel by inject()
+    override val layoutRes= R.layout.fragment_library_book
+    companion object {
+        fun newInstance(story: Story) = LibraryBookFragment(story)
+    }
+
+    override fun init() {
+        model.getTextStory(story)
+        model.getTitleStory(story)
+        iniViewModel()
+        back_button_character.setOnClickListener {backClicked()}
+    }
+
+    override fun iniViewModel() {
+        model.subscribeOnTextStory().observe(viewLifecycleOwner, Observer {textStory->
+            textStory?.let {text-> tv_tale.text=text }
+        })
+
+        model.subscribeOnTitleStory().observe(viewLifecycleOwner, Observer {titleStory->
+            titleStory?.let {title-> subHeader.text=title }
+        })
+    }
+
+    override fun backClicked(): Boolean {
+        router.exit()
+        return true
+    }
 }
