7429a2ffce26ea116bff0472e0a50767561d0662
Vitas1968 <dor1968@mail.ru>
2020-08-04 19:19:21 +0700

    экран библиотеки реализация

--- app/src/main/java/ru/storytellers/ui/fragments/LibraryFragment.kt
+++ app/src/main/java/ru/storytellers/ui/fragments/LibraryFragment.kt
@@ -1,30 +1,55 @@
 package ru.storytellers.ui.fragments
 
+import androidx.lifecycle.Observer
+import kotlinx.android.synthetic.main.fragment_library.*
 import org.koin.android.ext.android.inject
 import ru.storytellers.R
 import ru.storytellers.model.DataModel
 import ru.storytellers.model.entity.Story
+import ru.storytellers.navigation.Screens
+import ru.storytellers.ui.adapters.LibraryAdapter
 import ru.storytellers.ui.fragments.basefragment.BaseFragment
+import ru.storytellers.utils.toastShowLong
 import ru.storytellers.viewmodels.LibraryViewModel
 
-class LibraryFragment(
-    private val listStory:List<Story>
-): BaseFragment<DataModel>() {
+class LibraryFragment: BaseFragment<DataModel>() {
 
     override val model: LibraryViewModel by inject ()
     override val layoutRes = R.layout.fragment_library
 
     companion object {
-        fun newInstance(listStory:List<Story>) = LibraryFragment(listStory)
+        fun newInstance() = LibraryFragment()
     }
+    private val itemClickListener = object : LibraryAdapter.ItemClickListener {
+        override fun onItemClick(story: Story) {
+            navigateToLibraryBookScreen(story)
+        }
+    }
+    private val libraryAdapter: LibraryAdapter by lazy { LibraryAdapter(itemClickListener) }
 
 
     override fun init() {
-        TODO("Not yet implemented")
+        rv_books.adapter=libraryAdapter
+        iniViewModel()
+        model.getAllStory()
     }
 
     override fun iniViewModel() {
-        TODO("Not yet implemented")
+        model.subscribeOnSuccess().observe(viewLifecycleOwner, Observer {
+            it.data?.let {listStoryLocal->
+                if (listStoryLocal.isNotEmpty()) {
+                    libraryAdapter.setData(listStoryLocal)
+                }
+            }
+        })
+
+        model.subscribeOnError().observe(viewLifecycleOwner, Observer {
+            activity?.let { context -> toastShowLong(context,"Something went wrong") }
+        })
+    }
+
+    private fun navigateToLibraryBookScreen(story: Story){
+        router.navigateTo(Screens.LibraryBookScreen(story))
     }
 
     override fun backClicked(): Boolean {
