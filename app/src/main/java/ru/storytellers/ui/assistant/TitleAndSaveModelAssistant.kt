c09a7d9d4dac8343bd2d2520390fe046bee90643
Vitas1968 <dor1968@mail.ru>
2020-08-04 19:35:44 +0700

    почистил

--- app/src/main/java/ru/storytellers/ui/assistant/TitleAndSaveModelAssistant.kt
+++ app/src/main/java/ru/storytellers/ui/assistant/TitleAndSaveModelAssistant.kt
@@ -2,7 +2,6 @@ package ru.storytellers.ui.assistant
 
 import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers
 import io.reactivex.rxjava3.core.Completable
-import io.reactivex.rxjava3.core.Single
 import ru.storytellers.model.entity.Story
 import ru.storytellers.model.repository.IStoryRepository
 
@@ -10,21 +9,7 @@ import ru.storytellers.model.repository.IStoryRepository
 class TitleAndSaveModelAssistant(
     private val storyRepository: IStoryRepository
 ) {
-
-//    fun saveStory(story: Story):Boolean{
-//        var successSaveFlag:Boolean=false
-//        storyRepository.insertOrReplace(story)
-//            .observeOn(AndroidSchedulers.mainThread())
-//            .subscribe({
-//                successSaveFlag=true
-//            },{
-//                successSaveFlag=false
-//            })
-//        return successSaveFlag
-//    }
-
     fun saveStory(story: Story): Completable =
         storyRepository.insertOrReplace(story)
             .observeOn(AndroidSchedulers.mainThread())
-
 }
