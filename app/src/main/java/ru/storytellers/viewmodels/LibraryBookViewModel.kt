368b86fae536a104c791eb2571306c2c9016632f
Vitas1968 <dor1968@mail.ru>
2020-08-04 19:18:37 +0700

    модель экрана отображения сказки

--- app/src/main/java/ru/storytellers/viewmodels/LibraryBookViewModel.kt
+++ app/src/main/java/ru/storytellers/viewmodels/LibraryBookViewModel.kt
@@ -1,4 +1,34 @@
 package ru.storytellers.viewmodels
 
-class LibraryBookViewModel {
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import ru.storytellers.engine.GameStorage
+import ru.storytellers.model.DataModel
+import ru.storytellers.model.entity.Story
+import ru.storytellers.viewmodels.baseviewmodel.BaseViewModel
+import java.lang.StringBuilder
+
+class LibraryBookViewModel: BaseViewModel<DataModel>() {
+    private val textStoryLiveData = MutableLiveData<String>()
+    private val titleStoryLiveData = MutableLiveData<String>()
+
+    fun subscribeOnTextStory(): LiveData<String>{
+        return textStoryLiveData
+    }
+    fun subscribeOnTitleStory(): LiveData<String>{
+        return titleStoryLiveData
+    }
+
+     fun getTextStory(story: Story){
+        val textStory= StringBuilder()
+
+        story.sentences?.forEach {
+            textStory.append(it.content)
+        }
+        textStoryLiveData.value= textStory.toString()
+    }
+
+    fun getTitleStory(story: Story){
+        titleStoryLiveData.value=story.name
+    }
 }
