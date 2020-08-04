c90a1aaae657980de5861890df598c91222e4583
LeonovEgor <leonov.egor@gmail.com>
2020-08-04 21:16:13 +0300

    Добавлен Splash Screen

--- app/src/main/java/ru/storytellers/ui/SplashActivity.kt
+++ app/src/main/java/ru/storytellers/ui/SplashActivity.kt
@@ -12,21 +12,16 @@ class SplashActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        //hiding title bar of this activity
         window.requestFeature(Window.FEATURE_NO_TITLE)
-        //making this activity full screen
         window.setFlags(
             WindowManager.LayoutParams.FLAG_FULLSCREEN,
             WindowManager.LayoutParams.FLAG_FULLSCREEN
         )
         setContentView(R.layout.splash_activity)
 
-        //4second splash time
         Handler().postDelayed({
-            //start main activity
             startActivity(Intent(this@SplashActivity, MainActivity::class.java))
-            //finish this activity
             finish()
-        }, 10000)
+        }, 4000)
     }
 }
