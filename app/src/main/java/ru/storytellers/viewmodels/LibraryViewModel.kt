7a2727cca047bdf7cc789eb78fbe7db80b2e9f8f
Vitas1968 <dor1968@mail.ru>
2020-08-04 19:19:49 +0700

    модель экрана библиотеки

--- app/src/main/java/ru/storytellers/viewmodels/LibraryViewModel.kt
+++ app/src/main/java/ru/storytellers/viewmodels/LibraryViewModel.kt
@@ -1,7 +1,38 @@
 package ru.storytellers.viewmodels
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers
 import ru.storytellers.model.DataModel
+import ru.storytellers.model.entity.Story
+import ru.storytellers.model.repository.IStoryRepository
 import ru.storytellers.viewmodels.baseviewmodel.BaseViewModel
 
-class LibraryViewModel: BaseViewModel<DataModel>() {
+class LibraryViewModel(
+    private val storyRepository: IStoryRepository
+): BaseViewModel<DataModel>() {
+    private val onSuccessliveData = MutableLiveData<DataModel.Success<Story>>()
+    private val onErrorliveData = MutableLiveData<DataModel.Error>()
+    private val onLoadingliveData = MutableLiveData<DataModel.Loading>()
+
+    fun subscribeOnSuccess(): LiveData<DataModel.Success<Story>> {
+        return onSuccessliveData
+    }
+
+    fun subscribeOnError(): LiveData<DataModel.Error> {
+        return onErrorliveData
+    }
+    fun subscribeOnLoading() : LiveData<DataModel.Loading> {
+        return onLoadingliveData
+    }
+
+    fun getAllStory(){
+        storyRepository.getAll()
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribe({
+                onSuccessliveData.value=DataModel.Success(it)
+            },{
+                onErrorliveData.value=DataModel.Error(it)
+            })
+    }
 }
