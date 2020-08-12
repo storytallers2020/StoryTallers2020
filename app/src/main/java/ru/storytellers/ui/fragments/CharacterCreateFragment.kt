d2f4f4abd5002427d51433eee113c6064ee23ea8
Vitas1968 <dor1968@mail.ru>
2020-08-12 22:47:23 +0700

    извращался с получением цвета из ресурсов ну и по мелочам кое что добавил

--- app/src/main/java/ru/storytellers/ui/fragments/CharacterCreateFragment.kt
+++ app/src/main/java/ru/storytellers/ui/fragments/CharacterCreateFragment.kt
@@ -7,51 +7,64 @@ import android.view.View
 import android.view.inputmethod.InputMethodManager
 import androidx.lifecycle.Observer
 import kotlinx.android.synthetic.main.fragment_character_create.*
-import kotlinx.android.synthetic.main.fragment_game.*
-import kotlinx.android.synthetic.main.sentence_input_layout.*
 import org.koin.android.ext.android.inject
 import ru.storytellers.R
-import ru.storytellers.application.StoryTallerApp
 import ru.storytellers.model.DataModel
 import ru.storytellers.model.entity.Character
-import ru.storytellers.model.entity.Location
 import ru.storytellers.navigation.Screens
 import ru.storytellers.ui.adapters.CharacterCreateAdapter
-import ru.storytellers.ui.adapters.LocationAdapter
 import ru.storytellers.ui.fragments.basefragment.BaseFragment
-import ru.storytellers.utils.PlayerCreator
-import ru.storytellers.utils.fieldsToLogString
+import ru.storytellers.utils.toastShowLong
 import ru.storytellers.viewmodels.CharacterCreateViewModel
-import ru.storytellers.viewmodels.CreateCharacterViewModel
 import timber.log.Timber
 
 
-class CharacterCreateFragment: BaseFragment<DataModel>() {
+class CharacterCreateFragment : BaseFragment<DataModel>() {
     override val model: CharacterCreateViewModel by inject()
-    private var inputMethodManager: Any?= null
-    override val layoutRes= R.layout.fragment_character_create
-    private val onItemClickListener = { character: Character ->
+    private var inputMethodManager: Any? = null
+    override val layoutRes = R.layout.fragment_character_create
+
+    //  private val yellowColor=ResourcesCompat.getColor(resources,R.color.yellow,null)
+    //private val yellowColor=Color.parseColor(R.color.yellow.toString())
+    // private val yellowColor= context?.let {ContextCompat.getColor(it,R.color.yellow)  }
+    //private val noColor=context?.let {ContextCompat.getColor(it,R.color.no_color)}
+    private val onItemClickListener = { character: Character, itemRecycler: View, position: Int ->
         model.setCharacterOfPlayer(character)
+        //yellowColor?.let { itemRecycler.setBackgroundColor(it) }
+        // itemRecycler.setBackgroundColor(Color.parseColor(R.color.yellow.toString()))
+        itemRecycler.alpha = 0.3f
+        characterAdapter.notifyItemChanged(position)
     }
-    private val characterAdapter: CharacterCreateAdapter by lazy { CharacterCreateAdapter(onItemClickListener) }
+    private val characterAdapter: CharacterCreateAdapter by lazy {
+        CharacterCreateAdapter(
+            onItemClickListener
+        )
+    }
+
     companion object {
         fun newInstance() = CharacterCreateFragment()
     }
-    private  val textWatcher= object : TextWatcher {
+
+    private val textWatcher = object : TextWatcher {
         override fun onTextChanged(s: CharSequence, start: Int, before: Int, count: Int) {
-            if (s.length<2) { makeInVisibleBtnNext()}
+            if (s.length < 3) {
+                btn_next.isEnabled = false
+            }
         }
-        override fun beforeTextChanged(s: CharSequence, start: Int, count: Int, after: Int) {}
+
+        override fun beforeTextChanged(s: CharSequence, start: Int, count: Int, after: Int) {
+        }
+
         override fun afterTextChanged(text: Editable) {
-            if (text.toString().length>2) {
-                makeVisibleBtnNext()
+            if (text.toString().length > 2) {
+                btn_next.isEnabled = true
                 model.setNamePlayer(text.toString())
             }
         }
     }
 
     override fun init() {
-        rv_characters.adapter=characterAdapter
+        rv_characters.adapter = characterAdapter
         enter_name_field_et.addTextChangedListener(textWatcher)
         iniViewModel()
         btn_next.setOnClickListener { handlerBtnNext() }
@@ -59,10 +72,12 @@ class CharacterCreateFragment: BaseFragment<DataModel>() {
 
     override fun iniViewModel() {
         inputMethodManager = context?.getSystemService(Context.INPUT_METHOD_SERVICE)
-        screen_header.post{ View.FOCUS_DOWN }
+        screen_header.post { View.FOCUS_DOWN }
     }
 
     override fun onStart() {
+        val editable = Editable.Factory.getInstance().newEditable("")
+        enter_name_field_et.text = editable
         model.getAllCharacters()
         super.onStart()
     }
@@ -73,39 +88,36 @@ class CharacterCreateFragment: BaseFragment<DataModel>() {
         })
 
         model.subscribeOnSuccess().observe(viewLifecycleOwner, Observer {
-            it.let {
-                setDataCharacterAdapter(it)
-            }
+            setDataCharacterAdapter(it)
         })
         super.onResume()
     }
+
     private fun setDataCharacterAdapter(it: DataModel.Success<Character>) {
         if (it.data!!.isNotEmpty()) {
             characterAdapter.setData(it.data)
         }
     }
 
-    private fun hideKeyboard(){
-        (inputMethodManager as? InputMethodManager)?.
-        hideSoftInputFromWindow(enter_name_field_et.windowToken,
-            InputMethodManager.HIDE_NOT_ALWAYS)
-    }
-
-    private fun makeInVisibleBtnNext(){
-        btn_next.visibility= View.GONE
-    }
-    private fun makeVisibleBtnNext(){
-        btn_next.visibility= View.VISIBLE
+    private fun hideKeyboard() {
+        (inputMethodManager as? InputMethodManager)?.hideSoftInputFromWindow(
+            enter_name_field_et.windowToken,
+            InputMethodManager.HIDE_NOT_ALWAYS
+        )
     }
 
     override fun backClicked(): Boolean {
         router.exit()
         return true
     }
-    private fun handlerBtnNext(){
-        model.run{
-           addPlayer(createPlayer())
+
+    private fun handlerBtnNext() {
+        model.run {
+            getPlayer()?.let {
+                addPlayer(it)
+                router.navigateTo(Screens.TeamCharacterScreen())
+            } ?: context?.let { toastShowLong(it, "Персонаж не выбран!") }
         }
-        router.navigateTo(Screens.TeamCharacterScreen())
+
     }
 }
