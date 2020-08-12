caa9ee2c7f6bbace6c19ec68148c7cb1e457a344
Vitas1968 <dor1968@mail.ru>
2020-08-12 16:38:24 +0700

    реализация модели

--- app/src/main/java/ru/storytellers/viewmodels/TeamCharacterViewModel.kt
+++ app/src/main/java/ru/storytellers/viewmodels/TeamCharacterViewModel.kt
@@ -1,4 +1,25 @@
 package ru.storytellers.viewmodels
 
-class TeamCharacterViewModel {
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import ru.storytellers.application.StoryTallerApp
+import ru.storytellers.model.DataModel
+import ru.storytellers.model.entity.Player
+import ru.storytellers.viewmodels.baseviewmodel.BaseViewModel
+
+class TeamCharacterViewModel : BaseViewModel<DataModel>() {
+
+    private var listPlayers: MutableList<Player> =
+        StoryTallerApp.instance.gameStorage.getListPlayers()
+    private val playersLiveData = MutableLiveData<List<Player>>()
+
+    fun subscribeOnPlayers(): LiveData<List<Player>> {
+        playersLiveData.value = listPlayers
+        return playersLiveData
+    }
+
+    fun removePlayer(player: Player) {
+        listPlayers.remove(player)
+        playersLiveData.value = listPlayers
+    }
 }
