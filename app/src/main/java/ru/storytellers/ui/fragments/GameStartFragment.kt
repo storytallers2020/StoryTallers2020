9d328b0d583bf54ddf6c5fa8e485ddb3689642ac
Vitas1968 <dor1968@mail.ru>
2020-08-13 19:51:42 +0700

    реализация экрана

--- app/src/main/java/ru/storytellers/ui/fragments/GameStartFragment.kt
+++ app/src/main/java/ru/storytellers/ui/fragments/GameStartFragment.kt
@@ -1,27 +1,60 @@
 package ru.storytellers.ui.fragments
 
+import androidx.lifecycle.Observer
 import kotlinx.android.synthetic.main.fragment_game_start.*
+import org.koin.android.ext.android.inject
 import ru.storytellers.R
 import ru.storytellers.model.DataModel
 import ru.storytellers.navigation.Screens
 import ru.storytellers.ui.fragments.basefragment.BaseFragment
-import ru.storytellers.viewmodels.baseviewmodel.BaseViewModel
+import ru.storytellers.viewmodels.GameStartViewModel
 
-class GameStartFragment: BaseFragment<DataModel>() {
-    override val model: BaseViewModel<DataModel>
-        get() = TODO("Not yet implemented")
-    override val layoutRes=R.layout.fragment_game_start
+class GameStartFragment : BaseFragment<DataModel>() {
+    override val model: GameStartViewModel by inject()
+    override val layoutRes = R.layout.fragment_game_start
+
     companion object {
         fun newInstance() = GameStartFragment()
     }
 
     override fun init() {
-        btn_next.setOnClickListener { navigateToGameScreen() }
+        iniViewModel()
         back_button_location.setOnClickListener { backClicked() }
     }
 
     override fun iniViewModel() {
-        TODO("Not yet implemented")
+        btn_next.setOnClickListener { navigateToGameScreen() }
+    }
+
+    override fun onStart() {
+        super.onStart()
+        model.getLevelGame()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        model.subscribeOnLevelGame().observe(viewLifecycleOwner, Observer {
+            when (it) {
+                0 -> {
+                    getRuleFromResources(R.string.easy_description)
+                }
+                1 -> {
+                    getRuleFromResources(R.string.medium_description)
+                }
+                else -> {
+                    getRuleFromResources(R.string.hard_description)
+                }
+            }
+        })
+    }
+
+    private fun getRuleFromResources(stringId: Int?) {
+        var rule = stringId?.let { context?.getString(it) }
+        setRule(rule)
+    }
+
+    private fun setRule(rule: String?) {
+        rule?.let { text_rule_tv.text = it }
     }
 
     private fun navigateToGameScreen() {
