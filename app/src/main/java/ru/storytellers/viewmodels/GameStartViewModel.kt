f8afb685f93b25a03dc9315db61cdd6a1d032c63
Vitas1968 <dor1968@mail.ru>
2020-08-13 19:49:00 +0700

    реализация модели

--- app/src/main/java/ru/storytellers/viewmodels/GameStartViewModel.kt
+++ app/src/main/java/ru/storytellers/viewmodels/GameStartViewModel.kt
@@ -1,4 +1,16 @@
 package ru.storytellers.viewmodels
 
-class GameStartViewModel {
+import androidx.lifecycle.MutableLiveData
+import ru.storytellers.application.StoryTallerApp
+import ru.storytellers.model.DataModel
+import ru.storytellers.viewmodels.baseviewmodel.BaseViewModel
+
+class GameStartViewModel: BaseViewModel<DataModel>() {
+    private val levelGameLiveData = MutableLiveData<Int>()
+
+    fun getLevelGame(){
+        levelGameLiveData.value=StoryTallerApp.instance.gameStorage.getLevelGame()
+    }
+
+    fun subscribeOnLevelGame()=levelGameLiveData
 }
