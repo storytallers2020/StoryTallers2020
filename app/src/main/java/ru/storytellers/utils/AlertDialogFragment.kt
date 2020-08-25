842d8b190f381ec52084256e4a663ad419021b61
Vitas1968 <dor1968@mail.ru>
2020-08-26 01:58:15 +0700

    диалоговое окно удаления сказки

--- app/src/main/java/ru/storytellers/utils/AlertDialogFragment.kt
+++ app/src/main/java/ru/storytellers/utils/AlertDialogFragment.kt
@@ -7,32 +7,37 @@ import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatDialogFragment
 import androidx.fragment.app.FragmentActivity
 import ru.storytellers.R
+import ru.storytellers.model.entity.Story
 import ru.storytellers.ui.fragments.LibraryFragment
 
 class AlertDialogFragment(
     private val libraryFragment: LibraryFragment,
-    private val storyId:Long
-): AppCompatDialogFragment() {
+    private val story: Story
+) : AppCompatDialogFragment() {
 
     override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
         val context = libraryFragment.activity as FragmentActivity
-        return  getAlertDialog(context)
+        return getAlertDialog(context)
     }
 
     companion object {
-        fun newInstance(libraryFragment: LibraryFragment,storyId:Long) = AlertDialogFragment(libraryFragment,storyId)
+        fun newInstance(libraryFragment: LibraryFragment, story: Story) =
+            AlertDialogFragment(libraryFragment, story)
     }
-    private fun getAlertDialog(context: Context): AlertDialog {
-        val builder = context?.let { AlertDialog.Builder(it) }
-        builder?.apply {
-            setTitle(R.string.dialog_title)
-            setNegativeButton(R.string.negative_answer){dialog, _ -> dialog.dismiss()}
-            setCancelable(false)
-            setPositiveButton(R.string.positive_answer) { dialog, _ ->
-                libraryFragment.model.removeStory(storyId)
+
+    private fun getAlertDialog(context: Context) =
+        context?.let { AlertDialog.Builder(it) }
+            .setTitle(R.string.dialog_title)
+            .setCancelable(true)
+            .setNegativeButton(R.string.negative_answer) { dialog, _ ->
                 dialog.dismiss()
             }
-        }
-        return builder?.create()
-    }
+
+            .setPositiveButton(R.string.positive_answer) { dialog, _ ->
+                libraryFragment.model.removeStory(story)
+                dialog.dismiss()
+            }
+            .setPositiveButtonIcon(context.resources.getDrawable(R.drawable.ic_edit_bin))
+            .create()
+
 }
