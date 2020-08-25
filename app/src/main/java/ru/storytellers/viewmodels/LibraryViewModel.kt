1066af88f9e3ae65f42145b55c901778f1b477ca
Vitas1968 <dor1968@mail.ru>
2020-08-26 01:47:02 +0700

    функция удаления сказки, ливдата на результат удаления, подписка на ливдату

--- app/src/main/java/ru/storytellers/viewmodels/LibraryViewModel.kt
+++ app/src/main/java/ru/storytellers/viewmodels/LibraryViewModel.kt
@@ -8,42 +8,54 @@ import ru.storytellers.model.DataModel
 import ru.storytellers.model.entity.Story
 import ru.storytellers.model.repository.IStoryRepository
 import ru.storytellers.viewmodels.baseviewmodel.BaseViewModel
+import timber.log.Timber
 
 class LibraryViewModel(
     private val storyRepository: IStoryRepository
-): BaseViewModel<DataModel>() {
+) : BaseViewModel<DataModel>() {
     private val onSuccessliveData = MutableLiveData<DataModel.Success<Story>>()
     private val onErrorliveData = MutableLiveData<DataModel.Error>()
     private val onLoadingliveData = MutableLiveData<DataModel.Loading>()
-    private val gameStorage= StoryTallerApp.instance.gameStorage
+    private val onRemoveStoryLiveData = MutableLiveData<Int>()
+    private val gameStorage = StoryTallerApp.instance.gameStorage
 
     fun subscribeOnSuccess(): LiveData<DataModel.Success<Story>> {
         return onSuccessliveData
     }
 
+    fun subscribeRnRemoveStory(): LiveData<Int> {
+        return onRemoveStoryLiveData
+    }
+
     fun subscribeOnError(): LiveData<DataModel.Error> {
         return onErrorliveData
     }
-    fun subscribeOnLoading() : LiveData<DataModel.Loading> {
+
+    fun subscribeOnLoading(): LiveData<DataModel.Loading> {
         return onLoadingliveData
     }
 
-    fun getAllStory(){
+    fun getAllStory() {
         storyRepository.getAll()
             .observeOn(AndroidSchedulers.mainThread())
             .subscribe({
-                onSuccessliveData.value=DataModel.Success(it)
-            },{
-                onErrorliveData.value=DataModel.Error(it)
+                onSuccessliveData.value = DataModel.Success(it)
+            }, {
+                onErrorliveData.value = DataModel.Error(it)
             })
     }
 
-    fun removeStory(storyId:Long){
-        //storyRepository.removeStoryById(storyId)
+    fun removeStory(story: Story) {
+        storyRepository.delete(story).observeOn(AndroidSchedulers.mainThread())
+            .subscribe({
+                onRemoveStoryLiveData.value = it
+            }, {
+                Timber.e(it, "Remove story throwable")
+            })
     }
 
-     fun onClearStorage() {
-         gameStorage.apply {
+    fun onClearStorage() {
+        gameStorage.apply {
             clearTitleStory()
             clearCoverStoryTaller()
             clearLocationGame()
