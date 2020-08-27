f995a982edd3c888929bfd94e2a5bc8fef76c9ef
Vitas1968 <dor1968@mail.ru>
2020-08-27 19:53:26 +0700

    поправил класс под работц с двумя классами

--- app/src/main/java/ru/storytellers/utils/AlertDialogFragment.kt
+++ app/src/main/java/ru/storytellers/utils/AlertDialogFragment.kt
@@ -5,24 +5,26 @@ import android.app.Dialog
 import android.content.Context
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatDialogFragment
+import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentActivity
 import ru.storytellers.R
 import ru.storytellers.model.entity.Story
+import ru.storytellers.ui.fragments.LibraryBookFragment
 import ru.storytellers.ui.fragments.LibraryFragment
 
 class AlertDialogFragment(
-    private val libraryFragment: LibraryFragment,
+    private val fragment: Fragment,
     private val story: Story
 ) : AppCompatDialogFragment() {
 
     override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
-        val context = libraryFragment.activity as FragmentActivity
+        val context = fragment.activity as FragmentActivity
         return getAlertDialog(context)
     }
 
     companion object {
-        fun newInstance(libraryFragment: LibraryFragment, story: Story) =
-            AlertDialogFragment(libraryFragment, story)
+        fun newInstance(fragment: Fragment, story: Story) =
+            AlertDialogFragment(fragment, story)
     }
 
     private fun getAlertDialog(context: Context) =
@@ -31,7 +33,15 @@ class AlertDialogFragment(
             .setMessage(R.string.dialog_title)
             .setCancelable(true)
             .setNegativeButton(R.string.negative_answer) { dialog, _ -> dialog.dismiss() }
-            .setPositiveButton(R.string.positive_answer) { _, _ -> libraryFragment.model.removeStory(story) }
-            .create()
+            .setPositiveButton(R.string.positive_answer) { _, _ ->
+                when (fragment) {
+                    is LibraryFragment -> {
+                        fragment.model.removeStory(story)
+                    }
+                    is LibraryBookFragment -> {
+                        fragment.model.removeStory(story)
+                    }
+                }
+            }.create()
 
 }
