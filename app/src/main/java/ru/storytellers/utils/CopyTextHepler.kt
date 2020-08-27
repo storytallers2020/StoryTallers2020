ee895a3fe3bafbb386620a4fda2d20484a503148
Vitas1968 <dor1968@mail.ru>
2020-08-27 19:58:30 +0700

    вынес некоторые общие функции в utils

--- app/src/main/java/ru/storytellers/utils/CopyTextHepler.kt
+++ app/src/main/java/ru/storytellers/utils/CopyTextHepler.kt
@@ -3,6 +3,10 @@ package ru.storytellers.utils
 import android.content.ClipData
 import android.content.ClipboardManager
 import android.content.Context
+import android.content.Intent
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import ru.storytellers.R
 import timber.log.Timber
 
 fun String.setTextToClipboard(context: Context): Boolean {
@@ -17,3 +21,26 @@ fun String.setTextToClipboard(context: Context): Boolean {
 
     return true
 }
+
+fun shareText(fragment: Fragment, resultText: String) {
+    with(Intent()) {
+        action = Intent.ACTION_SEND
+        putExtra(Intent.EXTRA_TEXT, resultText)
+        type = "text/plain"
+        fragment.startActivity(Intent.createChooser(this, "Рассказать сказку..."))
+    }
+}
+
+fun copyText(context: Context, textStory: String) = textStory.setTextToClipboard(context)
+
+fun concatTitleAndTextStory(title: String, text: String, msgShare: String): String {
+    val concatText = StringBuilder()
+    return concatText.append(title)
+        .append("\n")
+        .append(text)
+        .append("\n")
+        .append(msgShare)
+        .toString()
+
+}
+
