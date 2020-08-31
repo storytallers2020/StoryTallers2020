04619b192b230ac0e898956bfc16271f10cb152f
Vitas1968 <dor1968@mail.ru>
2020-08-31 21:37:52 +0700

    реализация

--- app/src/main/java/ru/storytellers/ui/fragments/AppInfoFragment.kt
+++ app/src/main/java/ru/storytellers/ui/fragments/AppInfoFragment.kt
@@ -1,4 +1,48 @@
 package ru.storytellers.ui.fragments
 
-class AppInfoFragment {
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import org.koin.android.ext.android.inject
+import ru.storytellers.R
+import ru.storytellers.navigation.Screens
+import ru.storytellers.ui.BackButtonListener
+import ru.terrakok.cicerone.NavigatorHolder
+import ru.terrakok.cicerone.Router
+
+class AppInfoFragment : Fragment(), BackButtonListener {
+    private lateinit var navigatorHolder: NavigatorHolder
+    private lateinit var router: Router
+
+    companion object {
+        fun newInstance() = AppInfoFragment()
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        injectRouter()
+        return inflater.inflate(R.layout.fragment_rules, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        // back_button_rules.setOnClickListener { backClicked() }
+    }
+
+    private fun injectRouter() {
+        val navigHold: NavigatorHolder by inject()
+        navigatorHolder = navigHold
+        val rout: Router by inject()
+        router = rout
+    }
+
+    override fun backClicked(): Boolean {
+        router.replaceScreen(Screens.StartScreen())
+        return true
+    }
 }
