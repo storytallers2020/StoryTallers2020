9f9230befac7454ef29d3d73124dbd4980cf3ce6
Vitas1968 <dor1968@mail.ru>
2020-09-07 22:13:14 +0700

    реализация стат функции

--- app/src/main/java/ru/storytellers/viewmodels/LocationViewModel.kt
+++ app/src/main/java/ru/storytellers/viewmodels/LocationViewModel.kt
@@ -6,11 +6,17 @@ import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers
 import ru.storytellers.application.StoryTallerApp
 import ru.storytellers.model.DataModel
 import ru.storytellers.model.entity.Location
+import ru.storytellers.model.entity.Player
 import ru.storytellers.model.repository.ILocationRepository
+import ru.storytellers.utils.StatHelper
+import ru.storytellers.utils.getCurrentDateTime
+import ru.storytellers.utils.getString
+import ru.storytellers.utils.toProperties
 import ru.storytellers.viewmodels.baseviewmodel.BaseViewModel
 
 class LocationViewModel(private val locationRepository: ILocationRepository) :
     BaseViewModel<DataModel>() {
+    private val stat = StoryTallerApp.instance.stat
     private val onSuccessLiveData = MutableLiveData<DataModel.Success<Location>>()
     private val onErrorLiveData = MutableLiveData<DataModel.Error>()
 
@@ -34,6 +40,15 @@ class LocationViewModel(private val locationRepository: ILocationRepository) :
 
     fun setLocationGame(location: Location){
         StoryTallerApp.instance.gameStorage.setLocationGame(location)
+    }
+
+    fun onLocationChoiceStatistic(location: Location) {
+        val prop = listOf(
+            StatHelper.locationName to location.name,
+            StatHelper.locationId to location.id.toString(),
+            StatHelper.timeLocationChoice to getCurrentDateTime().getString()
+        )
+        stat.riseEvent(StatHelper.onLocationChoice, prop.toProperties())
     }
 
 }
