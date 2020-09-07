5c013340c2683c681db58f483da9e0b06646a597
Vitas1968 <dor1968@mail.ru>
2020-09-07 21:41:54 +0700

    константы для стартового экрана

--- app/src/main/java/ru/storytellers/utils/StatHelper.kt
+++ app/src/main/java/ru/storytellers/utils/StatHelper.kt
@@ -19,6 +19,16 @@ class StatHelper {
         const val onPlayerAdded = "onPlayerAdded"
         const val characterScreenNextClicked = "characterScreenNextClicked"
 
+        //StartScreen
+        const val onRulesGame = "onRulesGame"
+        const val userId = "userId"
+        const val timeOnRulesGame = "toRulesGameTime"
+        const val timeStartCreateTale = "timeStartCreateTale"
+        const val startCreateTale = "StartCreateTale"
+        const val onAboutScreen = "onAboutScreen"
+        const val onLibraryScreen = "toLibraryScreen"
+        const val timeToLibraryScreen = "timeToLibraryScreen"
+
 
     }
 }
