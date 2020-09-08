f9a811e192b39d99a1d86c5f78e024d87ccf1ed9
Vitas1968 <dor1968@mail.ru>
2020-09-08 23:09:33 +0700

    вызов функции статистики для кнопки Сохранить

--- app/src/main/java/ru/storytellers/ui/fragments/TitleAndSaveStoryFragment.kt
+++ app/src/main/java/ru/storytellers/ui/fragments/TitleAndSaveStoryFragment.kt
@@ -14,22 +14,23 @@ import ru.storytellers.utils.resourceToUri
 import ru.storytellers.utils.toastShowLong
 import ru.storytellers.viewmodels.TitleAndSaveStoryViewModel
 
-class TitleAndSaveStoryFragment:BaseFragment<DataModel>() {
+class TitleAndSaveStoryFragment : BaseFragment<DataModel>() {
     override val model: TitleAndSaveStoryViewModel by inject()
-    override val layoutRes= R.layout.fragment_choosing_title
+    override val layoutRes = R.layout.fragment_choosing_title
 
     companion object {
         fun newInstance() = TitleAndSaveStoryFragment()
     }
-    private val textWatcher= object : TextWatcher {
+
+    private val textWatcher = object : TextWatcher {
         override fun onTextChanged(s: CharSequence, start: Int, before: Int, count: Int) {}
         override fun beforeTextChanged(s: CharSequence, start: Int, count: Int, after: Int) {}
         override fun afterTextChanged(text: Editable) {
-            if(text.toString().length>1){
+            if (text.toString().length > 1) {
                 model.setTitleStory(text.toString())
-                btn_next.isEnabled=true
+                btn_next.isEnabled = true
             } else {
-                context?.let { toastShowLong(it,it.getString(R.string.enter_title)) }
+                context?.let { toastShowLong(it, it.getString(R.string.enter_title)) }
             }
         }
     }
@@ -38,7 +39,10 @@ class TitleAndSaveStoryFragment:BaseFragment<DataModel>() {
     override fun init() {
         book_name.addTextChangedListener(textWatcher)
         back_button_character.setOnClickListener { backToSelectCoverScreen() }
-        btn_next.setOnClickListener { saveStory() }
+        btn_next.setOnClickListener {
+            model.buttonSaveStoryClickedStatistic()
+            saveStory()
+        }
     }
 
     override fun onStart() {
@@ -54,25 +58,32 @@ class TitleAndSaveStoryFragment:BaseFragment<DataModel>() {
         })
 
         model.subscribeOnSuccessSaveFlag().observe(viewLifecycleOwner, Observer {
-            if(it) {
-                activity?.let {
-                    context -> toastShowLong(context,context.getString(R.string.msg_saved_successfully)) }
+            if (it) {
+                activity?.let { context ->
+                    toastShowLong(
+                        context,
+                        context.getString(R.string.msg_saved_successfully)
+                    )
+                }
                 navigateToLibraryScreen()
+            } else activity?.let { context ->
+                toastShowLong(
+                    context,
+                    context.getString(R.string.something_went_wrong)
+                )
             }
-
-
-            else  activity?.let {
-                    context -> toastShowLong(context,context.getString(R.string.something_went_wrong))}
         })
     }
-    private fun saveStory(){
+
+    private fun saveStory() {
         model.createStoryTaller()
     }
-    private fun navigateToLibraryScreen(){
+
+    private fun navigateToLibraryScreen() {
         router.navigateTo(Screens.LibraryScreen())
     }
 
-    private fun backToSelectCoverScreen(){
+    private fun backToSelectCoverScreen() {
         router.backTo(Screens.SelectCoverScreen())
     }
 
