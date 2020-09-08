2c68e8bbd0f9a322159643e991db38c7ddb212b6
Vitas1968 <dor1968@mail.ru>
2020-09-08 20:09:02 +0700

    реализация функций статистики

--- app/src/main/java/ru/storytellers/viewmodels/SelectCoverViewModel.kt
+++ app/src/main/java/ru/storytellers/viewmodels/SelectCoverViewModel.kt
@@ -7,11 +7,16 @@ import ru.storytellers.application.StoryTallerApp
 import ru.storytellers.model.DataModel
 import ru.storytellers.model.entity.Cover
 import ru.storytellers.model.repository.ICoverRepository
+import ru.storytellers.utils.StatHelper
+import ru.storytellers.utils.getCurrentDateTime
+import ru.storytellers.utils.getString
+import ru.storytellers.utils.toProperties
 import ru.storytellers.viewmodels.baseviewmodel.BaseViewModel
 
 class SelectCoverViewModel(
     private val coverRepository: ICoverRepository
 ): BaseViewModel<DataModel>() {
+    private val stat = StoryTallerApp.instance.stat
     private val onSuccessLiveData = MutableLiveData<DataModel.Success<Cover>>()
     private val onErrorLiveData = MutableLiveData<DataModel.Error>()
 
@@ -35,5 +40,16 @@ class SelectCoverViewModel(
 
     fun setCoverStory(cover: Cover){
         StoryTallerApp.instance.gameStorage.setCoverStoryTaller(cover)
+    }
+    fun coverStatistics(cover: Cover){
+        val prop = listOf(
+            StatHelper.coverName to cover.name,
+            StatHelper.coverId to cover.id.toString(),
+            StatHelper.selectCoverTime to getCurrentDateTime().getString()
+        )
+        stat.riseEvent(StatHelper.onSelectCoverScreen, prop.toProperties())
+    }
+    fun coverSelectedStatistics(){
+        stat.riseEvent(StatHelper.coverSelected)
     }
 }
