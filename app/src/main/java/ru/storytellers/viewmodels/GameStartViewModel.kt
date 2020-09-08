8502f7500f1d91bee2d9498e56dfa2c5b35ad473
Vitas1968 <dor1968@mail.ru>
2020-09-08 22:04:30 +0700

    функция статистики начала игры

--- app/src/main/java/ru/storytellers/viewmodels/GameStartViewModel.kt
+++ app/src/main/java/ru/storytellers/viewmodels/GameStartViewModel.kt
@@ -4,6 +4,10 @@ import androidx.lifecycle.MutableLiveData
 import ru.storytellers.application.StoryTallerApp
 import ru.storytellers.engine.Game
 import ru.storytellers.model.DataModel
+import ru.storytellers.utils.StatHelper
+import ru.storytellers.utils.getCurrentDateTime
+import ru.storytellers.utils.getString
+import ru.storytellers.utils.toProperties
 import ru.storytellers.viewmodels.baseviewmodel.BaseViewModel
 
 class GameStartViewModel(private val game: Game) : BaseViewModel<DataModel>() {
@@ -17,9 +21,29 @@ class GameStartViewModel(private val game: Game) : BaseViewModel<DataModel>() {
     fun subscribeOnLevelGame() = levelGameLiveData
 
     fun createNewGame() {
-        storage.level?.let {level ->
+        storage.level?.let { level ->
             game.newGame(storage.getPlayers(), level)
         }
+        gameStartStatistic()
+    }
+
+    private fun gameStartStatistic() {
+        val level = when (storage.level?.id) {
+            0 -> "Easy"
+            1 -> "Medium"
+            2 -> "Hard"
+            else -> "level not selected"
+        }
+        val prop = listOf(
+            StatHelper.numberOfPlayersGame to storage.getPlayers().count().toString(),
+            StatHelper.selectedLevelGame to level,
+            StatHelper.createGameTime to getCurrentDateTime().getString()
+        )
+        stat.riseEvent(StatHelper.onGameStartScreen, prop.toProperties())
+    }
+
+    fun buttonStartClickedStatistic() {
+        stat.riseEvent(StatHelper.buttonStartClicked)
     }
 
 }
