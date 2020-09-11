977874a0fe146640994c2df54916927c2015f491
Vitas1968 <dor1968@mail.ru>
2020-09-11 15:33:00 +0700

    сделал более читаемыми названия экранов и события, добавил событие на кли завершить игру и проперть-общее количество предложений в сказке

--- app/src/main/java/ru/storytellers/utils/StatHelper.kt
+++ app/src/main/java/ru/storytellers/utils/StatHelper.kt
@@ -9,57 +9,59 @@ class StatHelper {
         const val timeEvent= "TimeEvent"
         const val buttonBackClicked = "BackClicked"
         //StartScreen
-        const val startScreenBtnToRulesGame = "StartScreenBtnToRulesScreen"
+        const val startScreenBtnToRulesGame = "StartScreen|BtnToRulesScreen"
         const val userId = "userId"
-        const val startScreenBtnToCreateTale = "StartScreenBtnToCreateTale"
-        const val startScreenBtnToAboutScreen = "StartScreenBtnToAboutScreen"
-        const val startScreenBtnToLibraryScreen = "StartScreenBtnToLibraryScreen"
+        const val startScreenBtnToCreateTale = "StartScreen|BtnToCreateTale"
+        const val startScreenBtnToAboutScreen = "StartScreen|BtnToAboutScreen"
+        const val startScreenBtnToLibraryScreen = "StartScreen|BtnToLibraryScreen"
         //LevelScreen
-        const val levelScreenBtnToCharacterScreen = "LevelScreenBtnToCharacterScreen"
+        const val levelScreenBtnToCharacterScreen = "LevelScreen|BtnToCharacterScreen"
         const val levelName = "levelName"
         //CharacterCreateScreen
         const val playerName = "playerName"
         const val characterName = "characterName"
-        const val characterScreenBtnPlayerSaved = "CharacterScreenBtnPlayerSaved"
+        const val characterScreenBtnPlayerSaved = "CharacterScreen|BtnPlayerSaved"
         //TeamCharacterScreen
         const val playerId = "PlayerId"
         const val playerCount = "PlayerCount"
-        const val teamScreenAddPlayerClicked = "TeamScreenAddPlayerClicked"
-        const val teamScreenBtnToLocationScreen = "TeamScreenBtnToLocationScreen"
+        const val teamScreenAddPlayerClicked = "TeamScreen|AddPlayerClicked"
+        const val teamScreenBtnToLocationScreen = "TeamScreen|BtnToLocationScreen"
         //LocationScreen
-        const val locationScreenLocationSelected = "LocationScreenLocationSelected"
+        const val locationScreenLocationSelected = "LocationScreen|LocationSelected"
         const val locationName = "locationName"
         const val locationId = "locationId"
         //GameStartScreen
-        const val gameStartScreenBtnStartClicked = "GameStartScreenBtnStartClicked"
+        const val gameStartScreenBtnStartClicked = "GameStartScreen|BtnStartClicked"
         //GameScreen
         const val gamePlayerId = "GamePlayerId"
         const val gamePlayerName = "GamePlayerName"
-        const val gameScreenBtnSendClicked = "GameScreenBtnSendClicked"
+        const val gameScreenBtnSendClicked = "GameScreen|BtnSendClicked"
+        const val gameScreenBtnEndGameClicked = "GameScreen|BtnEndGameClicked"
+        const val totalNumberSentencesInStory = "TotalNumberSentencesInStory"
         const val turn = "Turn"
         //GameEndScreen
-        const val gameEndScreenBtnCopyClicked = "GameEndScreenBtnCopyClicked"
-        const val gameEndScreenBtnContinueGameClicked = "GameEndScreenBtnContinueGameClicked"
-        const val gameEndScreenBtnSelectCoverClicked = "GameEndScreenBtnSelectCoverClicked"
+        const val gameEndScreenBtnCopyClicked = "GameEndScreen|BtnCopyClicked"
+        const val gameEndScreenBtnContinueGameClicked = "GameEndScreen|BtnContinueGameClicked"
+        const val gameEndScreenBtnSelectCoverClicked = "GameEndScreen|BtnSelectCoverClicked"
         //SelectCoverScreen
-        const val selectCoverScreenCoverSelected = "SelectCoverScreenCoverSelected"
+        const val selectCoverScreenCoverSelected = "SelectCoverScreen|CoverSelected"
         const val coverName = "CoverName"
         const val coverId = "CoverId"
         //TitleAndSaveStoryScreen
-        const val titleAndSaveScreenBtnSaveClicked = "TitleAndSaveScreenBtnSaveClicked"
+        const val titleAndSaveScreenBtnSaveClicked = "TitleAndSaveScreen|BtnSaveClicked"
         const val storyId = "StoryId"
         const val storyName = "StoryName"
         const val saveStoryFailed = "SaveStoryFailed"
         //LibraryScreen
-        const val libraryScreenStorySelected = "LibraryScreenStorySelected"
-        const val libraryScreenMenuItemCopyClicked = "LibraryScreenMenuItemCopyClicked"
-        const val libraryScreenMenuItemDeleteClicked = "LibraryScreenMenuItemDeleteClicked"
-        const val libraryScreenMenuItemShareClicked = "LibraryScreenMenuItemShareClicked"
-        const val libraryScreenBtnStartScreenClicked = "LibraryScreenBtnStartScreenClicked"
+        const val libraryScreenStorySelected = "LibraryScreen|StorySelected"
+        const val libraryScreenMenuItemCopyClicked = "LibraryScreen|MenuItemCopyClicked"
+        const val libraryScreenMenuItemDeleteClicked = "LibraryScreen|MenuItemDeleteClicked"
+        const val libraryScreenMenuItemShareClicked = "LibraryScreen|MenuItemShareClicked"
+        const val libraryScreenBtnStartScreenClicked = "LibraryScreen|BtnStartScreenClicked"
         //LibraryBook
-        const val libraryBookScreenMenuItemCopyClicked = "LibraryBookScreenMenuItemCopyClicked"
-        const val libraryBookScreenMenuItemDeleteClicked = "LibraryBookScreenMenuItemDeleteClicked"
-        const val libraryBookScreenMenuItemShareClicked = "LibraryBookScreenMenuItemShareClicked"
+        const val libraryBookScreenMenuItemCopyClicked = "LibraryBookScreen|MenuItemCopyClicked"
+        const val libraryBookScreenMenuItemDeleteClicked = "LibraryBookScreen|MenuItemDeleteClicked"
+        const val libraryBookScreenMenuItemShareClicked = "LibraryBookScreen|MenuItemShareClicked"
 
          fun itemClickedStat(itemName:String){
             val prop = listOf(
