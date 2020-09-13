c051edb75991e2273582aff905d51991b3ded378
Katherine <katherine.zharkova@gmail.com>
2020-09-13 22:10:33 +0300

    versioning started

--- buildSrc/src/main/java/Dependencies.kt
+++ buildSrc/src/main/java/Dependencies.kt
@@ -9,8 +9,8 @@ object Config {
 }
 
 object Releases {
-    const val version_code = 1
-    const val version_name = "1.0"
+    const val version_code = 2
+    const val version_name = "0.0.1"
 }
 
 
