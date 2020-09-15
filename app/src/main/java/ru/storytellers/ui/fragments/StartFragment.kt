6eb045a7348db8d4fe8dee5f2c1e8f8503a03189
Katherine <katherine.zharkova@gmail.com>
2020-09-15 05:11:20 +0300

    added Agreement Dialog

--- app/src/main/java/ru/storytellers/ui/fragments/StartFragment.kt
+++ app/src/main/java/ru/storytellers/ui/fragments/StartFragment.kt
@@ -1,5 +1,6 @@
 package ru.storytellers.ui.fragments
 
+import android.content.SharedPreferences
 import android.view.View
 import androidx.lifecycle.Observer
 import kotlinx.android.synthetic.main.fragment_start.*
@@ -8,12 +9,17 @@ import ru.storytellers.R
 import ru.storytellers.model.DataModel
 import ru.storytellers.navigation.Screens
 import ru.storytellers.ui.fragments.basefragment.BaseFragment
+import ru.storytellers.utils.AlertDialogFragment
 import ru.storytellers.utils.toastShowLong
 import ru.storytellers.viewmodels.StartViewModel
+import timber.log.Timber
 
-class StartFragment: BaseFragment<DataModel>() {
+private const val FRAGMENT_DIALOG_TAG = "start-5d62-46bf-ab6"
+
+class StartFragment : BaseFragment<DataModel>() {
     override val layoutRes = R.layout.fragment_start
-    override  val model: StartViewModel by inject()
+    override val model: StartViewModel by inject()
+    private var sharedPreferences: SharedPreferences? = null
 
 
     companion object {
@@ -21,6 +27,7 @@ class StartFragment: BaseFragment<DataModel>() {
     }
 
     override fun init() {
+        checkAgreement()
         rules_button.setOnClickListener { navigateToRulesGame() }
         about_button.setOnClickListener { navigateToAboutScreen() }
         new_tale_button.setOnClickListener{ navigateToLevelScreen() }
@@ -44,24 +51,52 @@ class StartFragment: BaseFragment<DataModel>() {
 
     override fun iniViewModel() {
         model.subscribeOnSuccess().observe(viewLifecycleOwner, Observer {
-            it.data?.let {listStoryLocal->
+            it.data?.let { listStoryLocal ->
                 if (listStoryLocal.isNotEmpty()) {
-                    library_button.visibility= View.VISIBLE
+                    library_button.visibility = View.VISIBLE
                 }
             }
         })
 
         model.subscribeOnError().observe(viewLifecycleOwner, Observer {
-            activity?.let { context -> toastShowLong(context,context.getString(R.string.something_went_wrong)) }
+            activity?.let { context ->
+                toastShowLong(
+                    context,
+                    context.getString(R.string.something_went_wrong)
+                )
+            }
         })
     }
 
+    private fun checkAgreement() {
+        sharedPreferences = activity?.getSharedPreferences(FRAGMENT_DIALOG_TAG, 0)
+        sharedPreferences?.getBoolean(FRAGMENT_DIALOG_TAG, false)?.let {
+            if (!it) {
+                showAlertDialog()
+            }
+        }
+    }
 
-    private fun navigateToRulesGame(){
+    private fun showAlertDialog() {
+        activity?.supportFragmentManager?.let { fragMan ->
+            AlertDialogFragment.newInstance(this).show(fragMan, FRAGMENT_DIALOG_TAG)
+        }
+    }
+
+    fun acceptAgreement() {
+        sharedPreferences?.edit()?.putBoolean(FRAGMENT_DIALOG_TAG, true)?.apply()
+        Timber.e(
+            "Alert: AgreementAccepted: %s",
+            sharedPreferences?.getBoolean(FRAGMENT_DIALOG_TAG, false)
+        )
+    }
+
+    private fun navigateToRulesGame() {
         model.toRulesScreenStatistics()
         router.navigateTo(Screens.RulesGameScreen())
     }
-    private fun navigateToAboutScreen(){
+
+    private fun navigateToAboutScreen() {
         model.onAboutScreenStatistics()
         router.navigateTo(Screens.AppInfoScreen())
     }
