c10d2ad59744936d73317de497db7c93fc8cdca8
Vitas1968 <dor1968@mail.ru>
2020-09-17 23:01:37 +0700

    проверка на длинну строки и на то что строка не состоит из одних пробелов

--- app/src/main/java/ru/storytellers/utils/InputValidation.kt
+++ app/src/main/java/ru/storytellers/utils/InputValidation.kt
@@ -1,4 +1,32 @@
 package ru.storytellers.utils
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+
 class InputValidation {
+
+    private val onInputIncorrectLiveData = MutableLiveData<Int>()
+
+    fun subscribeOnInputIncorrect(): LiveData<Int> {
+        return onInputIncorrectLiveData
+    }
+
+     fun inputValidation(content:String): Int {
+        val flagInputIsNormal = 0 // проходит валидацию
+        val flagInputIsShort = 1 // входная строка короткая,меньще двух символов
+        val flagInputIsGaps = 2 // входная строка состоит из одних пробелов
+        if (content.length < 2) {
+            setValueInputIncorrectLiveData(flagInputIsShort)
+            return flagInputIsShort }
+        if (content.stringNotContainSymbols()) {
+            setValueInputIncorrectLiveData(flagInputIsGaps)
+            return flagInputIsGaps
+        }
+         setValueInputIncorrectLiveData(flagInputIsNormal)
+        return flagInputIsNormal
+    }
+    private fun setValueInputIncorrectLiveData(value:Int){
+        onInputIncorrectLiveData.value=value
+
+    }
 }
