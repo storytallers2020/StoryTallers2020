39de4421ee76ddae74d94a9c75ba9b1cd2296494
Katherine <katherine.zharkova@gmail.com>
2020-09-19 05:26:36 +0300

    inflated about screen

--- app/src/main/java/ru/storytellers/ui/fragments/AppInfoFragment.kt
+++ app/src/main/java/ru/storytellers/ui/fragments/AppInfoFragment.kt
@@ -1,9 +1,11 @@
 package ru.storytellers.ui.fragments
 
 import android.os.Bundle
+import android.text.method.LinkMovementMethod
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.fragment.app.Fragment
 import kotlinx.android.synthetic.main.fragment_start_about.*
 import org.koin.android.ext.android.inject
@@ -14,8 +16,8 @@ import ru.terrakok.cicerone.NavigatorHolder
 import ru.terrakok.cicerone.Router
 
 class AppInfoFragment : Fragment(), BackButtonListener {
-    private lateinit var navigatorHolder: NavigatorHolder
-    private lateinit var router: Router
+    private val navigatorHolder: NavigatorHolder by inject()
+    private val router: Router by inject()
 
     companion object {
         fun newInstance() = AppInfoFragment()
@@ -26,20 +28,33 @@ class AppInfoFragment : Fragment(), BackButtonListener {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        injectRouter()
         return inflater.inflate(R.layout.fragment_start_about, container, false)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-         back_button_about.setOnClickListener { backClicked() }
+        setAllLinksClickable(view)
+        back_button_about.setOnClickListener { backClicked() }
     }
 
-    private fun injectRouter() {
-        val navigHold: NavigatorHolder by inject()
-        navigatorHolder = navigHold
-        val rout: Router by inject()
-        router = rout
+    private fun setAllLinksClickable(v: View): ArrayList<View>? {
+        if (v !is ViewGroup) {
+            return ArrayList<View>().apply { add(v) }
+        }
+
+        return ArrayList<View>().apply {
+            for (i in 0 until v.childCount) {
+                val child = v.getChildAt(i)
+                if (child is TextView) {
+                    child.movementMethod = LinkMovementMethod.getInstance()
+                }
+                ArrayList<View>().apply {
+                    add(v)
+                    addAll(setAllLinksClickable(child)!!)
+                    addAll(this)
+                }
+            }
+        }
     }
 
     override fun backClicked(): Boolean {
