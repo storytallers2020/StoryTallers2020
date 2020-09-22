eed51a83c3f4b7283d8659fc83eb4b386258ef18
Falchio <53504233+Falchio@users.noreply.github.com>
2020-09-22 21:11:48 +0300

    Закончил CharacterCreateViewModelTest.kt

--- app/src/test/java/ru/storytellers/viewmodels/CharacterCreateViewModelTest.kt
+++ app/src/test/java/ru/storytellers/viewmodels/CharacterCreateViewModelTest.kt
@@ -5,15 +5,12 @@ import android.text.Editable
 import android.text.SpannableStringBuilder
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
-import io.mockk.*
-import io.mockk.impl.annotations.SpyK
+import io.mockk.every
+import io.mockk.mockk
+import io.mockk.verify
 import io.reactivex.rxjava3.core.Single
-import org.junit.After
-import org.junit.Before
-
+import org.junit.*
 import org.junit.Assert.*
-import org.junit.Rule
-import org.junit.Test
 import org.junit.runner.RunWith
 import org.koin.core.context.loadKoinModules
 import org.koin.core.context.stopKoin
@@ -21,12 +18,12 @@ import org.koin.test.KoinTest
 import org.robolectric.Shadows
 import org.robolectric.annotation.Config
 import org.robolectric.annotation.LooperMode
+import ru.storytellers.di.amplitudeModule
 import ru.storytellers.di.gameEngine
 import ru.storytellers.model.entity.Character
 import ru.storytellers.model.entity.Player
 import ru.storytellers.model.repository.CharacterRepository
 import ru.storytellers.utils.PlayerCreator
-import ru.storytellers.utils.trimSentenceSpace
 
 @RunWith(AndroidJUnit4::class)
 @LooperMode(LooperMode.Mode.PAUSED)
@@ -43,7 +40,7 @@ class CharacterCreateViewModelTest : KoinTest {
 
     @Before
     fun setUp() {
-        loadKoinModules(gameEngine)
+        loadKoinModules(listOf(gameEngine, amplitudeModule))
         characterCreateViewModel = CharacterCreateViewModel(characterRepository, playerCreator)
     }
 
@@ -89,18 +86,32 @@ class CharacterCreateViewModelTest : KoinTest {
     }
 
     @Test
-    fun `gg`() {
-        val editable: Editable = SpannableStringBuilder("Editable")
-        val player = mockk<Player>()
-        val editableValue = editable.toString()
-//        every { playerCreator.setNamePlayer(editableValue.trimSentenceSpace()) } returns Unit
-//        every { playerCreator.getPlayer() } returns player
+    fun `when called CharacterCreateViewModel-transferNamePlayer called and setNamePlayer + getPlayer,`() {
 
+        val editable: Editable = SpannableStringBuilder("Editable")
+        val char = mockk<Character>()
+        val name = "Slim_Shady"
+        val player = Player(1, name, char)
+
+        every { char.name } returns name
+        every { playerCreator.setCharacterOfPlayer(char) } returns Unit
+        every { playerCreator.setNamePlayer(editable.toString()) } returns Unit
+        every { playerCreator.getPlayer() } returns player
+
+        characterCreateViewModel.characterSelected(char)
         characterCreateViewModel.transferNamePlayer(editable)
 
-//        verify {
-////            playerCreator.setNamePlayer(editableValue.trimSentenceSpace())
-//            playerCreator.getPlayer()
-//        }
+        verify {
+            playerCreator.setNamePlayer(editable.toString())
+            playerCreator.getPlayer()
+        }
+    }
+
+    @Test
+    fun `character was selected`() {
+        val char = mockk<Character>()
+        every { playerCreator.setCharacterOfPlayer(char) } returns Unit
+        characterCreateViewModel.characterSelected(char)
+        assertTrue(characterCreateViewModel.subscribeOnCharacterSelected().value!!)
     }
 }
