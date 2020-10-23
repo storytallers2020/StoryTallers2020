0385a6973905710830bf3eac985d88725e128576
PAVEL <zoom0071947@mail.ru>
2020-10-23 20:05:26 +0300

    Сделаны подсказки, для тех кто играет впервые.

--- app/src/main/java/ru/storytellers/utils/AlertDialogFragment.kt
+++ app/src/main/java/ru/storytellers/utils/AlertDialogFragment.kt
@@ -3,6 +3,7 @@ package ru.storytellers.utils
 import android.app.AlertDialog
 import android.app.Dialog
 import android.content.Context
+import android.content.Intent
 import android.os.Bundle
 import android.text.method.LinkMovementMethod
 import android.widget.TextView
@@ -10,7 +11,9 @@ import androidx.appcompat.app.AppCompatDialogFragment
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentActivity
 import ru.storytellers.R
+import ru.storytellers.ui.StepActivity
 import ru.storytellers.ui.fragments.*
+
 
 class AlertDialogFragment(private val fragment: Fragment, private val title: Int) : AppCompatDialogFragment() {
 
@@ -72,7 +75,12 @@ class AlertDialogFragment(private val fragment: Fragment, private val title: Int
                 .setPositiveButton(R.string.accept_answer) { dialog, _ ->
                     toastShowShort(context, getString(R.string.msg_agreement_accepted))
                     fragment.acceptAgreement()
+                    val intent = Intent(context, StepActivity::class.java)
+                    intent.flags = Intent.FLAG_ACTIVITY_SINGLE_TOP
+                    context.startActivity(intent)
                     dialog.dismiss()
+
+
                 }
                 .create()
         } else {
