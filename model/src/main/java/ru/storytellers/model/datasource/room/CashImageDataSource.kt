73bea4ee8b2c1c484c17d221977bd411990c77c1
LeonovEgor <leonov.egor@gmail.com>
2020-11-28 14:40:51 +0300

    Промежуточный коммит. Для взятия данных с сервера

--- model/src/main/java/ru/storytellers/model/datasource/room/CashImageDataSource.kt
+++ model/src/main/java/ru/storytellers/model/datasource/room/CashImageDataSource.kt
@@ -2,20 +2,23 @@ package ru.storytellers.model.datasource.room
 
 import io.reactivex.rxjava3.core.Completable
 import io.reactivex.rxjava3.schedulers.Schedulers
+import ru.storytellers.model.cache.IImageCache
 import ru.storytellers.model.datasource.ICashImageDataSource
 import ru.storytellers.model.entity.room.RoomCachedImage
 import ru.storytellers.model.entity.room.db.AppDatabase
 import ru.storytellers.utils.downloadImage
 import java.io.File
 
-class CashImageDataSource(private val database: AppDatabase, private val dir: File) :
-    ICashImageDataSource {
+class CashImageDataSource(
+    private val database: AppDatabase,
+    private val imageCache: IImageCache)
+    : ICashImageDataSource {
 
     override fun add(urlList: List<String>): Completable =
         Completable.create { emitter ->
             urlList.map { url ->
-                downloadImage(url, dir)
-                database.imageDao.insert(RoomCachedImage(url, imageFile.path))
+                downloadImage(url)
+                database.imageDao.insert(RoomCachedImage(url, url.remoteUrlToLocalPath()))
             }
             emitter.onComplete()
         }.subscribeOn(Schedulers.io())
--- model/src/main/java/ru/storytellers/utils/ImageHelper.kt
+++ model/src/main/java/ru/storytellers/utils/ImageHelper.kt
@@ -1,21 +1,19 @@
 package ru.storytellers.utils
 
-import android.app.Application
 import android.content.Context
+import android.graphics.Bitmap
 import android.graphics.drawable.Drawable
 import androidx.core.graphics.drawable.toBitmap
 import com.bumptech.glide.Glide
 import com.bumptech.glide.request.target.CustomTarget
 import com.bumptech.glide.request.transition.Transition
-import ru.storytellers.model.entity.room.RoomCachedImage
+import ru.storytellers.model.cache.IImageCache
 import timber.log.Timber
-import java.io.File
-import java.io.FileOutputStream
-import java.io.IOException
+import java.io.ByteArrayOutputStream
 
 
-fun downloadImage(url: String, dir: String) {
-    Glide.with(. context)
+fun downloadImage(url: String, context: Context, imageCache: IImageCache) {
+    Glide.with(context)
         .asDrawable()
         .load(url)
         .into(object : CustomTarget<Drawable>(){
@@ -24,7 +22,12 @@ fun downloadImage(url: String, dir: String) {
             override fun onResourceReady(resource: Drawable, transition: Transition<in Drawable>?) {
                 val image = resource.toBitmap()
                 try {
-                    saveImage(url, image)
+                    Timber.d("Сохранение изображения $url на устройстве")
+                    imageCache.saveImage(url, image.toByteArray()).subscribe({
+                        Timber.d("loadInto Image saved")
+                    }, {
+                        Timber.e(it)
+                    })
                 } catch(t: Throwable) {
                     Timber.e(t, "Ошибка сохранения картинок на устройстве")
                 }
@@ -32,19 +35,9 @@ fun downloadImage(url: String, dir: String) {
         })
 }
 
-private fun saveImage(url: String, bytes: ByteArray) {
-    if (!dir.exists() && !dir.mkdirs()) {
-        throw IOException("Failed to create cache! Dir: ${dir.absolutePath}")
-    }
-
-    val imageFile = File(dir, url.toMD5() + Png)
-    Timber.d("$dir ${url.toMD5()} + $Png")
-
-    FileOutputStream(imageFile).use { stream ->
-        stream.write(bytes)
-    }
-    Timber.d("saveImage: image saved. Try to add image info to database")
-
-
-    Timber.d("saveImage: image info added to database")
+//TODO: Вынести в helper?
+private fun Bitmap.toByteArray(): ByteArray {
+    val stream = ByteArrayOutputStream()
+    this.compress(Bitmap.CompressFormat.PNG, 100, stream)
+    return stream.use { it.toByteArray() }
 }
