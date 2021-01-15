6d7bff48190c0a74450d7a142d7d888d2b4ab24c
LeonovEgor <leonov.egor@gmail.com>
2021-01-15 19:46:12 +0300

    merge

--- model/src/main/java/ru/storytellers/model/datasource/room/CashImageDataSource.kt
+++ model/src/main/java/ru/storytellers/model/datasource/room/CashImageDataSource.kt
@@ -7,19 +7,56 @@ import ru.storytellers.model.datasource.ICashImageDataSource
 import ru.storytellers.model.entity.room.RoomCachedImage
 import ru.storytellers.model.entity.room.db.AppDatabase
 import ru.storytellers.utils.downloadImage
+import ru.storytellers.utils.toMD5
+import timber.log.Timber
 import java.io.File
+import java.io.FileOutputStream
+import java.io.IOException
 
-class CashImageDataSource(
-    private val database: AppDatabase,
-    private val imageCache: IImageCache)
+class CashImageDataSource(private val database: AppDatabase, private val dir: File)
+//    private val database: AppDatabase,
+//    private val imageCache: IImageCache)
     : ICashImageDataSource {
+
+//    override fun add(urlList: List<String>): Completable =
+//        Completable.create { emitter ->
+//            urlList.map { url ->
+//                downloadImage(url)
+//                database.imageDao.insert(RoomCachedImage(url, url.remoteUrlToLocalPath()))
+//            }
+//            emitter.onComplete()
+//        }.subscribeOn(Schedulers.io())
+
+    private val Png = ".png"
 
     override fun add(urlList: List<String>): Completable =
         Completable.create { emitter ->
-            urlList.map { url ->
-                downloadImage(url)
-                database.imageDao.insert(RoomCachedImage(url, url.remoteUrlToLocalPath()))
+            urlList.map {
+                val image = getRemoteImage(it).toByteArray()
+                try {
+                    saveImage(it, image)
+                } catch(t: Throwable) {
+                    Timber.e(t, "Ошибка сохранения картинок на устройстве")
+                }
             }
             emitter.onComplete()
         }.subscribeOn(Schedulers.io())
+
+    //TODO: Переместить в ImageHelper
+    private fun saveImage(url: String, bytes: ByteArray) {
+        if (!dir.exists() && !dir.mkdirs()) {
+            throw IOException("Failed to create cache! Dir: ${dir.absolutePath}")
+        }
+
+        val imageFile = File(dir, url.toMD5() + Png)
+        Timber.d("$dir ${url.toMD5()} + $Png")
+
+        FileOutputStream(imageFile).use { stream ->
+            stream.write(bytes)
+        }
+        Timber.d("saveImage: image saved. Try to add image info to database")
+
+        database.imageDao.insert(RoomCachedImage(url, imageFile.path))
+        Timber.d("saveImage: image info added to database")
+    }
 }
