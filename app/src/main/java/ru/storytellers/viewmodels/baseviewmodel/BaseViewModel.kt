97f72b8e4b4096bed30670a867936f8ea055356e
Vitas1968 <dor1968@mail.ru>
2021-02-10 21:34:35 +0700

    в базовую модель добавляем ливдату для управления прогрессбаром и функции подписки и управлением значения ливдаты

--- app/src/main/java/ru/storytellers/viewmodels/baseviewmodel/BaseViewModel.kt
+++ app/src/main/java/ru/storytellers/viewmodels/baseviewmodel/BaseViewModel.kt
@@ -1,14 +1,30 @@
 package ru.storytellers.viewmodels.baseviewmodel
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import ru.storytellers.application.StoryHeroesApp
 import ru.storytellers.model.DataModel
+import ru.storytellers.model.entity.Character
 import ru.storytellers.utils.StatHelper
 
 abstract class BaseViewModel<T : DataModel>(
 ): ViewModel() {
+    protected  val onProgressEnableLiveData = MutableLiveData<Boolean>()
     fun onBackClicked(fragmentName: String) {
         StoryHeroesApp.instance
             .stat.riseEvent("$fragmentName : ${StatHelper.buttonBackClicked}")
+    }
+
+    protected fun setFalseInProgressEnableLiveData(){
+        onProgressEnableLiveData.value=false
+    }
+
+    protected fun setTrueInProgressEnableLiveData(){
+        onProgressEnableLiveData.value=true
+    }
+
+     fun subscribeOnProgressEnableLiveData(): LiveData<Boolean> {
+        return onProgressEnableLiveData
     }
 }
