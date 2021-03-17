8e977c595a059a2b29fcac52215103791071dc94
Vitas1968 <dor1968@mail.ru>
2021-03-17 23:26:07 +0700

    добавлены функции обработки данных авторизации и в статистику добавил userName

--- app/src/main/java/ru/storytellers/viewmodels/StartViewModel.kt
+++ app/src/main/java/ru/storytellers/viewmodels/StartViewModel.kt
@@ -3,24 +3,36 @@ package ru.storytellers.viewmodels
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
+import com.google.android.gms.tasks.Task
 import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers
 import ru.storytellers.application.StoryHeroesApp
 import ru.storytellers.model.DataModel
 import ru.storytellers.model.entity.Story
+import ru.storytellers.model.entity.UserAccount
 import ru.storytellers.model.repository.IStoryRepository
+import ru.storytellers.utils.SignInGoogleHandler
 import ru.storytellers.utils.StatHelper
 import ru.storytellers.utils.StatHelper.Companion.riseEvent
 import ru.storytellers.utils.getCurrentDateTime
 import ru.storytellers.utils.getString
 import ru.storytellers.viewmodels.baseviewmodel.BaseViewModel
+import timber.log.Timber
 
 
 class StartViewModel(
-    private val storyRepository: IStoryRepository
+    private val storyRepository: IStoryRepository,
+    private val signInGoogleHandler: SignInGoogleHandler
 ) : BaseViewModel<DataModel>() {
     private val onSuccessLiveData = MutableLiveData<DataModel.Success<Story>>()
     private val onErrorLiveData = MutableLiveData<DataModel.Error>()
     private val onLoadingLiveData = MutableLiveData<DataModel.Loading>()
+    private val accountExistsLiveData = MutableLiveData<Boolean>()
+    private var userAccount: UserAccount? = null
+
+    fun subscribeOnAccountExists(): LiveData<Boolean> {
+        return accountExistsLiveData
+    }
 
     fun subscribeOnSuccess(): LiveData<DataModel.Success<Story>> {
         return onSuccessLiveData
@@ -34,6 +46,10 @@ class StartViewModel(
         return onLoadingLiveData
     }
 
+    fun checkLastSignedInAccount(account: GoogleSignInAccount?) {
+        accountExistsLiveData.value = account?.let { true } ?: false
+    }
+
     fun toRulesScreenStatistics() {
         val prop = listOf(
             StatHelper.userId to "userId", // заглушка пока что
@@ -43,10 +59,14 @@ class StartViewModel(
     }
 
     fun createTaleStatistics() {
-        val prop = listOf(
-            StatHelper.timeEvent to getCurrentDateTime().getString(),
-            StatHelper.userId to "userId" // заглушка пока что
-        )
+        val prop = listOf<Pair<String, String>>()
+        userAccount?.let {
+            with(prop) {
+                plus(StatHelper.timeEvent to getCurrentDateTime().getString())
+                plus(StatHelper.userId to it.id)
+                plus(StatHelper.userName to it.name)
+            }
+        }
         riseEvent(StatHelper.startScreenBtnToCreateTale, prop)
     }
 
@@ -57,7 +77,7 @@ class StartViewModel(
         riseEvent(StatHelper.startScreenBtnToLibraryScreen, prop)
     }
 
-    fun timeCreateStory(){
+    fun timeCreateStory() {
         StoryHeroesApp.instance.gameStorage.setTimeCreateStory(getCurrentDateTime().time)
     }
 
@@ -68,11 +88,16 @@ class StartViewModel(
         riseEvent(StatHelper.startScreenBtnToAboutScreen, prop)
     }
 
-    fun onStartScreenNumberOfTaleStat(numberOfTale:Int) {
+    fun onStartScreenNumberOfTaleStat(numberOfTale: Int) {
         val prop = listOf(
             StatHelper.startScreenNumberOfTale to numberOfTale.toString()
         )
         riseEvent(StatHelper.startScreen, prop)
+    }
+
+    fun getUserAccount(completedTask: Task<GoogleSignInAccount>) {
+        userAccount = signInGoogleHandler.getUserAccount(completedTask)
+        Timber.d(userAccount?.name)
     }
 
     fun getAllStory() {
