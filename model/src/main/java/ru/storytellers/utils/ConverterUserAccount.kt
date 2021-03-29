98928d4fbbabdfdb10b55672d543dcb0b01a01c9
Vitas1968 <dor1968@mail.ru>
2021-03-29 19:37:45 +0700

    убрал лишнее

--- model/src/main/java/ru/storytellers/utils/ConverterUserAccount.kt
+++ model/src/main/java/ru/storytellers/utils/ConverterUserAccount.kt
@@ -1,27 +1,15 @@
 package ru.storytellers.utils
 
-import android.net.Uri
 import ru.storytellers.model.datasource.remote.api.UserAccountApi
 import ru.storytellers.model.entity.UserAccount
 
 fun convertAccountToAccountApi(userAccount: UserAccount) =
     userAccount.run {
         UserAccountApi(
-            nameUser = name,
-            emailUser = email,
-            idUser = id,
+            name = name,
             idToken = idToken,
-            photoUrlUser = photoUrl.toString()
+            avatarUrl = photoUrl.toString()
         )
     }
 
-fun convertAccountApiToAccount(userAccountApi: UserAccountApi) =
-    userAccountApi.run {
-        UserAccount(
-            name = nameUser,
-            email = emailUser,
-            id = idUser,
-            idToken = idToken,
-            photoUrl = photoUrlUser  as Uri
-        )
-    }
+
