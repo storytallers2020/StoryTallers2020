623b1fa7e37744cfe0a15fcc6e0278fb30466048
Vitas1968 <dor1968@mail.ru>
2021-03-29 19:32:55 +0700

    переделал

--- model/src/main/java/ru/storytellers/model/datasource/remote/api/UserAccountApi.kt
+++ model/src/main/java/ru/storytellers/model/datasource/remote/api/UserAccountApi.kt
@@ -4,9 +4,7 @@ package ru.storytellers.model.datasource.remote.api
 import com.google.gson.annotations.Expose
 
 data class UserAccountApi(
-    @Expose val nameUser: String,
-    @Expose val emailUser: String,
-    @Expose val idUser: String,
+    @Expose val name: String,
     @Expose val idToken: String,
-    @Expose val photoUrlUser: String
+    @Expose val avatarUrl: String
 )
