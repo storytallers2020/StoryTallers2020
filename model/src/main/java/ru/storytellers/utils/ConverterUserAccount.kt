cf6b8eeb2241040bb0223af35c28ed4142504e7e
Vitas1968 <dor1968@mail.ru>
2021-04-08 19:03:36 +0700

    рефакторинг конвертеров

--- model/src/main/java/ru/storytellers/utils/ConverterUserAccount.kt
+++ model/src/main/java/ru/storytellers/utils/ConverterUserAccount.kt
@@ -1,15 +1,29 @@
 package ru.storytellers.utils
 
-import ru.storytellers.model.datasource.remote.api.UserAccountApi
+import ru.storytellers.model.datasource.remote.api.UserApi
+import ru.storytellers.model.entity.User
 import ru.storytellers.model.entity.UserAccount
 
-fun convertAccountToAccountApi(userAccount: UserAccount) =
+
+fun convertUserToUserApi(user : User) : UserApi =
+user.run{
+    UserApi(
+        name = name,
+        userId = id,
+        avatarUrl = avatarUrl
+    )
+}
+
+fun convertAccountToUser(userAccount: UserAccount) : User =
     userAccount.run {
-        UserAccountApi(
+        User(
+            id = email.hashCode().toLong(),
             name = name,
-            idToken = idToken,
+            email = email,
             avatarUrl = photoUrl.toString()
         )
     }
+
+
 
 
