411397f445c4596541b6ba315445c92b34f4340a
Vitas1968 <dor1968@mail.ru>
2021-04-08 18:57:24 +0700

    функции получения User от backend by id, функции получения всех Users

--- model/src/main/java/ru/storytellers/model/datasource/remote/IRemoteDataSource.kt
+++ model/src/main/java/ru/storytellers/model/datasource/remote/IRemoteDataSource.kt
@@ -4,14 +4,9 @@ import io.reactivex.rxjava3.core.Completable
 import io.reactivex.rxjava3.core.Single
 import org.jetbrains.annotations.NotNull
 import retrofit2.Response
-import retrofit2.http.Body
-import retrofit2.http.GET
-import retrofit2.http.Headers
-import retrofit2.http.POST
-import ru.storytellers.model.datasource.remote.api.CharactersListApi
-import ru.storytellers.model.datasource.remote.api.CoverListApi
-import ru.storytellers.model.datasource.remote.api.LocationListApi
-import ru.storytellers.model.datasource.remote.api.UserAccountApi
+import retrofit2.http.*
+import ru.storytellers.model.datasource.remote.api.*
+import ru.storytellers.model.entity.User
 import ru.storytellers.model.entity.UserAccount
 
 interface IRemoteDataSource {
@@ -25,5 +20,11 @@ interface IRemoteDataSource {
     fun getCovers(): Single<CoverListApi>
 
     @POST("v1/full-data/")
-    fun saveUser(@Body userAccount : UserAccountApi) : Completable
+    fun saveUser(@Body user : UserApi) : Completable
+
+    @GET("v1/getUserId/")
+    fun getUserById(@Query("userId") userId : Long ) : Single<UserApi>
+
+    @GET("v1/getUserId/")
+    fun getAllUser() : Single<List<UserAccountApi>>
 }
