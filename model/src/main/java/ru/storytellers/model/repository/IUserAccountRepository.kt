908f2f984b85565eae76c01984daafe3f36f9912
Vitas1968 <dor1968@mail.ru>
2021-04-08 19:01:28 +0700

    рефакторинг IUserAccountRepository

--- model/src/main/java/ru/storytellers/model/repository/IUserAccountRepository.kt
+++ model/src/main/java/ru/storytellers/model/repository/IUserAccountRepository.kt
@@ -3,8 +3,10 @@ package ru.storytellers.model.repository
 import io.reactivex.rxjava3.core.Completable
 import io.reactivex.rxjava3.core.Single
 import org.jetbrains.annotations.NotNull
-import ru.storytellers.model.entity.UserAccount
+import ru.storytellers.model.datasource.remote.api.UserApi
+import ru.storytellers.model.entity.User
 
 interface IUserAccountRepository {
-    fun saveUser(userAccount : UserAccount) : @NotNull Completable
+    fun saveUser(user : User) : @NotNull Completable
+    fun getUser(user : User) : Single<UserApi>
 }
