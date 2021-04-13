b839f95dcab9887360c0ca5bb402cd8b5fb66935
LeonovEgor <leonov.egor@gmail.com>
2021-04-13 20:00:41 +0300

    - удален проект resources

--- resources/src/main/java/ru/storytellers/utils/ResourceHelper.kt
+++
@@ -1,7 +0,0 @@
-package ru.storytellers.utils
-
-import android.content.ContentResolver
-import android.content.Context
-import android.net.Uri
-
-//fun resourceToUri(url: String): Uri? = Uri.parse(url)
