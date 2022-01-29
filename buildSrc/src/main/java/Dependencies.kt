1a193e86c489d5e835fd79b2ee647bc2ccf5d9ba
Vitaly Dorofeev <dor.1968.terminator@gmail.com>
2022-01-29 23:15:48 +0700

    обновление версии gradle, compileSdkVersion

--- buildSrc/src/main/java/Dependencies.kt
+++ buildSrc/src/main/java/Dependencies.kt
@@ -2,9 +2,9 @@ import org.gradle.api.JavaVersion
 
 object Config {
     const val application_id = "ru.storytellers"
-    const val compile_sdk = 29
+    const val compile_sdk = 31
     const val min_sdk = 21
-    const val target_sdk = 29
+    const val target_sdk = 30
     val java_version = JavaVersion.VERSION_1_8
 }
 
